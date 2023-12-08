93354857768ea44d7e0bb866961d0c403f4a4768
igor161rus <dranyi.igor@gmail.com>
2023-12-08 16:19:42 +0300

    lesson 4

--- lesson_004/02/02_name_space.py
+++ lesson_004/02/02_name_space.py
@@ -1,8 +1,33 @@
 def test_function():
     def inner_function():
         print('Я в области видимости функции test_function')
-
     inner_function()
 
 
+def test_function2():
+    pass
 
+
+func_list = [name for (name, obj) in vars().items()
+             if hasattr(obj, "__class__") and obj.__class__.__name__ == "function"]
+# lst_fn = []
+# for (name, obj) in vars().items():
+#     if hasattr(obj, "__class__") and obj.__class__.__name__ == "function":
+#         lst_fn.append(name)
+# print(lst_fn)
+
+print(func_list)
+
+try:
+    inner_function()
+except NameError:
+    print('Нет такого объекта на этом уровне')
+    print('Объявлены только: ')
+    for i, j in enumerate(func_list):
+        print(j)
+
+
+# if 'inner_function' in globals():
+#     print('Нет такого объекта на этом уровне')
+# else:
+#     test_function()
--- lesson_004/test.py
+++ lesson_004/test.py
@@ -1,4 +1,30 @@
-def func(x, *args, **kwargs):
-    print(args, kwargs)
+# def func(x, *args, **kwargs):
+#     print(args, kwargs)
+#
+# func(1, 2, 3, 4)
 
-func(1, 2, 3, 4)
+# if 'jhjj' == (i := input()):
+#     print('ok')
+
+def scope_test():
+    def do_local():
+        spam = "local spam"
+
+    def do_nonlocal():
+        nonlocal spam
+        spam = "nonlocal spam"
+
+    def do_global():
+        global spam
+        spam = "global spam"
+
+    spam = "test spam"
+    do_local()
+    print("After local assignment:", spam)
+    do_nonlocal()
+    print("After nonlocal assignment:", spam)
+    do_global()
+    print("After global assignment:", spam)
+
+scope_test()
+print("In global scope:", spam)
