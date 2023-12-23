fb7bd57154f427c46876927a44643b92de42e494
igor161rus <dranyi.igor@gmail.com>
2023-12-23 12:49:13 +0300

    Домашняя работа по уроку "Различие атрибутов класса и экземпляра."

--- lesson_005/04_difference_class_ and_instance_attributes.py
+++ lesson_005/04_difference_class_ and_instance_attributes.py
@@ -1,13 +1,23 @@
 class Building:
     total = 0
 
-    def __init__(self):
+    def __init__(self, n):
         Building.total += 1
-        # print(self.total)
+        self.total = n
+
+    def __str__(self):
+        return "bulding_" + str(self.total)
+
+    __repr__ = __str__
 
 
 buildings = []
 for i in range(40):
-    my_building = Building()
+    my_building = Building(i)
     buildings.append(my_building)
 print(buildings)
+
+# for i in range(40):
+#     my_building = Building()
+#     print(my_building.total)
+#     print(my_building)
