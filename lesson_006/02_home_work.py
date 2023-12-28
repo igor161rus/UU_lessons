259dddac373b227c2a5bc1fa9aaaedab2ea30e67
igor161rus <dranyi.igor@gmail.com>
2023-12-28 13:59:27 +0300

    Урок 6 ДЗ

--- lesson_006/02_home_work.py
+++ lesson_006/02_home_work.py
@@ -5,33 +5,47 @@
 # функцию horse_powers Получившийся код прикрепите к заданию текстом
 
 class Car:
-    # price = 1000000
-    # hp = 100
+    price = 0
 
     def __init__(self):
-        self.price = 1000000
-        self.hp = None
+        self.hp = 0
 
-    def horse_powers(self):
-        self.hp = 100
+    def horse_powers(self, hp):
+        self.hp = hp
+        return self.hp
+
+    def set_price(self):
+        pass
 
     def __str__(self):
-        return '{} {} {}'.format(self.__class__.__name__, self.price, self.hp)
+        return 'авто {}, стоимость {}, {} л/с '.format(self.__class__.__name__, self.price, self.hp)
 
 
 class Nissan(Car):
-    def horse_powers(self):
-        self.hp = 150
+    price = self.price
+    def horse_powers(self, hp):
+        self.hp = hp
+        power = hp // 1.35962
+        return self.hp, power
 
 
 class Kia(Car):
-    price = 1500000
-
-    def horse_powers(self):
-        hp = 180
+    def horse_powers(self, hp):
+        self.hp = hp
+        power = hp // 1.35962
+        return self.hp, power
 
 
 my_car = Nissan()
 print(my_car)
-my_car.horse_powers()
-print(my_car.hp)
+my_car.price = 1500000
+ret = my_car.horse_powers(160)
+print('авто {}, {} л/с, {} квт'.format(my_car.__class__.__name__, ret[0], ret[1]))
+print(my_car)
+
+my_car2 = Kia()
+print(my_car2)
+my_car2.price = 1200000
+ret = my_car2.horse_powers(105)
+print('авто {}, {} л/с, {} квт'.format(my_car2.__class__.__name__, ret[0], ret[1]))
+print(my_car2)
