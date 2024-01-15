8297822350648c5790026336bb225f0c7224edad
igor161rus <dranyi.igor@gmail.com>
2024-01-15 14:58:51 +0300

    Практика.

--- lesson_007/practice.py
+++ lesson_007/practice.py
@@ -2,6 +2,7 @@ import zipfile
 from pprint import pprint
 from random import randint
 
+
 # zip_file_name = 'voyna-i-mir.zip'
 #
 # zfile = zipfile.ZipFile(zip_file_name, 'r')
@@ -12,19 +13,20 @@ from random import randint
 
 class Chatterer:
     analize_count = 4
-    def __init__(self, zip_file_name):
-        self.zip_file_name = zip_file_name
+
+    def __init__(self, file_name):
+        self.file_name = file_name
         self.stat = {}
 
     def unzip(self):
-         zfile = zipfile.ZipFile(self.zip_file_name, 'r')
-         for filename in zfile.namelist():
-             zfile.extract(filename)
-         return filename
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
 
-    def collect(self, filename):
+    def collect(self):
         sequence = ' ' * self.analize_count
-        with open(filename, 'r', encoding='cp1251') as file:
+        with open(self.file_name, 'r', encoding='cp1251') as file:
             for line in file:
                 line = line[:-1]
                 # print(line)
@@ -49,61 +51,41 @@ class Chatterer:
                 self.stat_for_generate[sequence].append([count, char])
             self.stat_for_generate[sequence].sort(reverse=True)
 
-# filename = 'voyna-i-mir.txt'
-# stat = {}
-# stat = {'а':{'т': 500, 'х': 5, }, '':{'о': 100, 'у': 50, }}
+    def chat(self, N, out_file_name=None):
+        N = 1000
+        printed = 0
+        if out_file_name is not None:
+            file = open(out_file_name, 'w')
+        else:
+            file = None
 
-# analize_count = 4
-# sequence = ' ' * analize_count
-# with open(filename, 'r', encoding='cp1251') as file:
-#     for line in file:
-#         line = line[:-1]
-#         # print(line)
-#         for char in line:
-#             if sequence in stat:
-#                 if char in stat[sequence]:
-#                     stat[sequence][char] += 1
-#                 else:
-#                     stat[sequence][char] = 1
-#             else:
-#                 stat[sequence] = {char: 1}
-#             sequence = sequence[1:] + char
-#
-# pprint(stat)
-# pprint(len(stat))
+        sequence = ' ' * self.analize_count
+        spaces_printed = 0
+        while printed < N:
+            char_stat = self.stat_for_generate[sequence]
+            total = self.totals[sequence]
+            dice = randint(1, total)
+            pos = 0
+            for count, char in char_stat:
+                pos += count
+                if dice <= pos:
+                    break
+            if file:
+                file.write(char)
+            else:
+                print(char, end='')
+            if char == ' ':
+                spaces_printed += 1
+                if spaces_printed >= 10:
+                    print()
+                    spaces_printed = 0
+            printed += 1
+            sequence = sequence[1:] + char
+        if file:
+            file.close()
 
-# totals = {}
-# stat_for_generate = {}
-# for sequence, char_stat in stat.items():
-#     totals[sequence] = 0
-#     stat_for_generate[sequence] = []
-#     for char, count in char_stat.items():
-#         totals[sequence] += count
-#         stat_for_generate[sequence].append([count, char])
-#     stat_for_generate[sequence].sort(reverse=True)
 
-# pprint(totals)
-# pprint(stat_for_generate)
-
-N = 1000
-printed = 0
-
-sequence = ' ' * analize_count
-spaces_printed = 0
-while printed < N:
-    char_stat = stat_for_generate[sequence]
-    total = totals[sequence]
-    dice = randint(1, total)
-    pos = 0
-    for count, char in char_stat:
-        pos += count
-        if dice <= pos:
-            break
-    print(char, end='')
-    if char == ' ':
-        spaces_printed += 1
-        if spaces_printed >= 10:
-            print()
-            spaces_printed = 0
-    printed += 1
-    sequence = sequence[1:] + char
+chattere = Chatterer(file_name='voyna-i-mir.txt')
+chattere.collect()
+chattere.prepare()
+chattere.chat(N=10000, out_file_name='out.txt')
