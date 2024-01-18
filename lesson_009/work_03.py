62ca5f10d929a00bb9a6d2440a9658d04402ac77
igor161rus <dranyi.igor@gmail.com>
2024-01-18 14:41:46 +0300

    Генерация функций.

--- lesson_009/work_03.py
+++ lesson_009/work_03.py
@@ -17,3 +17,20 @@ result = map(by_2, my_numbers)
 print(list(result))
 result = map(by_3, my_numbers)
 print(list(result))
+
+
+def get_multiplier_v2(n):
+    def multiplier(x):
+        return x * n
+
+    return multiplier
+
+
+by_5 = get_multiplier_v2(5)
+print(by_5(x=42))
+result = map(by_5, my_numbers)
+print(list(result))
+by_100 = get_multiplier_v2(100)
+result = map(by_100, my_numbers)
+print(list(result))
+
