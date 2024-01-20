f3a34cfc0f7d9ee85cfed4bdc112bca7067caaab
igor161rus <dranyi.igor@gmail.com>
2024-01-20 15:59:02 +0300

    Генераторы.

--- lesson_009/work_05.py
+++ lesson_009/work_05.py
@@ -9,3 +9,19 @@ fib = fibonacchi_v2(n=10)
 print(fib)
 for value in fib:
     print(value)
+print()
+print('****************************************')
+print()
+
+
+def fibonacci_v3():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+
+for value in fibonacci_v3():
+    print(value)
+    if value > 10 ** 6:
+        break
