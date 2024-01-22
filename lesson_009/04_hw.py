b119704d311eb5d0e3d1bf466b386a9507e675f3
igor161rus <dranyi.igor@gmail.com>
2024-01-22 14:58:20 +0300

    Домашнее задание по теме "Генераторы"

--- lesson_009/04_hw.py
+++ lesson_009/04_hw.py
@@ -15,13 +15,41 @@
 # bc
 # abc
 
+
 def all_variants(in_str):
+    j = 0
     len_str = len(in_str)
-    for i in range(len_str):
-        for j in range(len_str - 1):
-            yield in_str[i]
+    for i in in_str:
+        yield i
+    while j < len_str-1:
+        yield in_str[j] + in_str[j + 1]
+        j += 1
+    yield in_str
 
 
 a = all_variants("abc")
 for i in a:
     print(i)
+
+# all_variants("abc")
+# a
+# b
+# c
+# ab
+# bc
+# abc
+
+
+# all_variants("abcdef")
+# a
+# b
+# c
+# d
+# e
+# f
+# ab
+# bc
+# cd
+# de
+# ef
+# abcdef
