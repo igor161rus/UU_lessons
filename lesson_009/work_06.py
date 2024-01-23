02b66e8a552abf0953a737bb776b1414f39a4751
igor161rus <dranyi.igor@gmail.com>
2024-01-23 09:16:50 +0300

    Декораторы.

--- lesson_009/work_06.py
+++ lesson_009/work_06.py
@@ -62,3 +62,29 @@ def digits_v2(*args):
 
 result = digits(3141, 5926, 2718, 2818)
 print(result)
+
+print()
+print('*************************************')
+print()
+
+
+def func_decorator(func):
+    def wrapper(*args, **kwargs):
+        print('----- что-то делаем до вызова функции -----')
+        res = func(*args, **kwargs)
+        print('----- что-то делаем после вызова функции -----')
+        return res
+
+    return wrapper
+
+
+@func_decorator
+def some_func(title, tag):
+    print(f'title = {title}, tag = {tag}')
+    return f'<{tag}>{title}</{tag}>'
+
+some_func('Python навсегда', 'h1')
+# some_func = func_decorator(some_func)
+# res = some_func('Python навсегда', 'h1')
+# print(res)
+
