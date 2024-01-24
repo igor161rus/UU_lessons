9a58690b953311bb91b14f394a0f4c48645e4fd7
igor161rus <dranyi.igor@gmail.com>
2024-01-24 22:43:59 +0300

    Декораторы с параметрами. Сохранение свойств декорируемых функций

--- lesson_009/work_add_02.py
+++ lesson_009/work_add_02.py
@@ -1,8 +1,34 @@
-def func_decorator(func):
-    def wrapper(*args, **kwargs):
-        print('----- что-то делаем до вызова функции -----')
-        res = func(*args, **kwargs)
-        print('----- что-то делаем после вызова функции -----')
-        return res
+import math
+from functools import wraps
 
-    return wrapper
+
+def df_decorator(dx=0.01):
+    def func_decorator(func):
+        @wraps(func)
+        def wrapper(x, *args, **kwargs):
+            res = (func(x + dx, *args, **kwargs) - func(x, *args, **kwargs)) / dx
+            return res
+
+        # wrapper.__name__ = func.__name__
+        # wrapper.__doc__ = func.__doc__
+        return wrapper
+
+    return func_decorator
+
+
+@df_decorator(dx=0.001)
+def sin_df(x):
+    """Функция для вычесления производной синуса"""
+    return math.sin(x)
+
+
+# f = df_decorator(dx=0.001)
+# sin_df = f(sin_df)
+#
+# sin_df = df_decorator(dx=0.001)(sin_df)
+
+print(sin_df.__name__)
+print(sin_df.__doc__)
+
+df = sin_df(math.pi / 3)
+print(df)
