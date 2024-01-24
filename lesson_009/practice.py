ff70ad3b29906aa1c287cf103c91f3d2d49ef9a5
igor161rus <dranyi.igor@gmail.com>
2024-01-24 23:21:50 +0300

    Практика

--- lesson_009/practice.py
+++ lesson_009/practice.py
@@ -22,3 +22,9 @@ stutters = [shutter_factory(n=n) for n in range(1, 4)]
 print(stutters)
 for func in stutters:
     print(func(animal))
+result = [func(animal) for func in stutters]
+print(result)
+
+animals = ['мика', 'бибимот', 'агавка', 'папакан', 'колик', 'соловель']
+mesh = [func(animal) for animal in animals for func in stutters]
+print(mesh)
