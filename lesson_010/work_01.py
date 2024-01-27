4ef7d3bb13bfbd4b26285dfba5bde3e2db94a87f
igor161rus <dranyi.igor@gmail.com>
2024-01-27 16:26:17 +0300

    Как создать и запустить поток.

---
+++ lesson_010/work_01.py
@@ -0,0 +1,19 @@
+import random
+from collections import defaultdict
+
+FISH = (None, 'плотва', 'окунь', 'лещ')
+
+def fishing(name, worms):
+    catch = defaultdict(int)
+    for worm in range(worms):
+        print(f'{name}: Червяк № {worm} - Забросил, ждем...', flush=True)
+        _ = 3 ** (random.randint(50, 70) * 10000)
+        fish = random.choice(FISH)
+        if fish is None:
+            print(f'{name}: Тьфу, сожрали червяка...', flush=True)
+        else:
+            print(f'Ага, у меня {fish}', flush=True)
+            catch[fish] += 1
+    print(f'Итого рыбак {name} поймал:')
+    for fish, count in catch.items():
+        print(f'   {fish} - {count}')
