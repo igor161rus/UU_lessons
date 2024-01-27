73c47aff8005d8cf969bcf07eb25403986d020ef
igor161rus <dranyi.igor@gmail.com>
2024-01-27 15:22:43 +0300

    Домашнее задание по теме "Декораторы"

---
+++ lesson_009/05_hw.py
@@ -0,0 +1,43 @@
+# Задание:
+# Напишите 2 функции:
+# Функция которая складывает 3 числа (sum_three)
+# Функция декоратор (is_prime), которая распечатывает "Простое", если результат 1ой функции будет простым числом и
+# "Составное" в противном случае.
+
+# Пример:
+# result = sum_three(2, 3, 6)
+# print(result)
+#
+# Результат консоли:
+# Простое
+# 11
+#
+# Примечание:
+# Не забудьте написать внутреннюю функцию wrapper в is_prime
+# Функция is_prime должна возвращать wrapper
+# @is_prime - декоратор для функции sum_three
+
+def is_prime(func):
+    def wrapper(*args, **kwargs):
+        res = func(*args, **kwargs)
+        if res % 2 == 0:
+            print('Простое' if res == 2 else 'Составное')
+            return res
+        d = 3
+        while d * d <= res and res % d != 0:
+            d += 2
+        print('Простое' if d * d > res else 'Составное')
+        return res
+    return wrapper
+
+
+@is_prime
+def sum_three(*args):
+    res = 0
+    for arg in args:
+        res += arg
+    return res
+
+
+result = sum_three(1, 6, 2, 7, 8)
+print(result)
