5559a9852fd5bc249618cfa72046453efc17c790
igor161rus <dranyi.igor@gmail.com>
2024-01-30 16:23:40 +0300

    Домашнее задание по теме "Создание потоков".

---
+++ lesson_010/01_hw.py
@@ -0,0 +1,93 @@
+# Задание:
+# Напишите программу, которая создает два потока.
+# Первый поток должен выводить числа от 1 до 10 с интервалом в 1 секунду.
+# Второй поток должен выводить буквы от 'a' до 'j' с тем же интервалом.
+# Оба потока должны работать параллельно.
+#
+# Примечание:
+# Используйте методы: start() для старта потока, join() для заморозки дальнейшей интерпретации,
+# пока процессы не завершаться.
+# Для установки интервала в 1 секунду используйте функцию sleep() из модуля time, предварительно импортировав его.
+#
+#
+# Входные данные:
+# Нет
+# Выходные данные:
+# 1
+# a
+# 2
+# b
+# 3
+# c
+# ......
+# 10
+# j
+
+import time
+from threading import Thread
+
+num_list = (i for i in range(1, 11))
+letter_list = (chr(a) for a in range(97, 107))
+
+
+def get_time_track(func):
+    def wrapper(*args, **kwargs):
+        strted_at = time.time()
+        result = func(*args, **kwargs)
+        ended_at = time.time()
+        elapsed = round(ended_at - strted_at, 6)
+        print(f'{func.__doc__} работала {elapsed} секунд(ы)')
+        return result
+
+    return wrapper
+
+
+@get_time_track
+def thread_numb():
+    """Функция вывода букв от 'a' до 'j' с интервалом в 1 секунду"""
+    for i in num_list:
+        print(i, flush=True)
+        time.sleep(1)
+
+
+@get_time_track
+def thread_letter():
+    """Функция вывода букв от 'a' до 'j' с интервалом в 1 секунду"""
+    for i in letter_list:
+        print(i, flush=True)
+        time.sleep(1)
+
+
+thread_1 = Thread(target=thread_numb)
+thread_2 = Thread(target=thread_letter)
+
+thread_1.start()
+thread_2.start()
+
+thread_1.join()
+thread_2.join()
+
+
+# v2
+@get_time_track
+def func_out(nl):
+    """Функция вывода цифр и букв"""
+    if nl == 0:
+        for i in num_list:
+            print(i, flush=True)
+            time.sleep(1)
+    elif nl == 1:
+        for i in letter_list:
+            print(i, flush=True)
+            time.sleep(1)
+
+
+num_list = (i for i in range(1, 11))
+letter_list = (chr(a) for a in range(97, 107))
+
+thread = Thread(target=func_out, args=(0,))
+thread.start()
+thread = Thread(target=func_out, args=(1,))
+thread.start()
+
+thread.join()
