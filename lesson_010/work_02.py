0486cb89f0f8d60a81409cca40b410b237537dc3
igor161rus <dranyi.igor@gmail.com>
2024-01-31 12:20:31 +0300

    Потоки на классах.

---
+++ lesson_010/work_02.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+from random import random
+from threading import Thread
+
+FISH = (None, 'плотва', 'окунь', 'лещ')
+
+
+class Fisher(Thread):
+    def __init__(self, name, worms, *args, **kwargs):
+        super(Fisher, self).__init__(*args, **kwargs)
+        self.name = name
+        self.worms = worms
+
+    def run(self):
+        catch = defaultdict(int)
+        for worm in range(self.worms):
+            print(f'{self.name}: Червяк № {worm} - Забросил, ждем...', flush=True)
+            _ = 3 ** (random.randint(50, 70) * 10000)
+            fish = random.choice(FISH)
+            if fish is None:
+                print(f'{self.name}: Тьфу, сожрали червяка...', flush=True)
+            else:
+                print(f'{self.name}: Ага, у меня {fish}', flush=True)
+                catch[fish] += 1
+        print(f'Итого рыбак {self.name} поймал:', flush=True)
+        for fish, count in catch.items():
+            print(f'   {fish} - {count}', flush=True)
+
+
+vasya = Fisher(name='Вася', worms=10)
+kolya = Fisher(name='Коля', worms=10)
+
+print('.' * 20, 'Они пошли на рыбалку')
+
+vasya.start()
+kolya.start()
+
+print('.' * 20, 'Ждем пока они вернуться...')
+
+vasya.join()
+kolya.join()
+print(print('.' * 20, 'Итак они вернулись'))
