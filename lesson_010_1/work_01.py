fd20b0acd7ef4791a8abb0fc7403a5685a1c0706
igor161rus <dranyi.igor@gmail.com>
2024-02-07 12:32:08 +0300

    Очереди для обмена данными между потоками.

--- lesson_010_1/work_01.py
+++ lesson_010_1/work_01.py
@@ -1,3 +1,4 @@
+import time
 from collections import defaultdict
 import random
 import queue
@@ -7,21 +8,64 @@ FISH = (None, 'плотва', 'окунь', 'лещ')
 
 
 class Fisher(Thread):
-    def __init__(self, name, worms, fish_tank, *args, **kwargs):
+    def __init__(self, name, worms, catcher, *args, **kwargs):
         super(Fisher, self).__init__(*args, **kwargs)
         self.name = name
         self.worms = worms
-        self.fish_tank = fish_tank
+        self.catcher = catcher
 
     def run(self):
         for worm in range(self.worms):
             print(f'{self.name}, {worm} - забросили, ждем...', flush=True)
+            time.sleep(random.randint(5, 20) / 10)
             fish = random.choice(FISH)
             if fish is None:
                 print(f'{self.name}, {worm}: сожрали червяка...', flush=True)
             else:
                 print(f'{self.name}, {worm}: поймал {fish} и хочет посадить его в садок', flush=True)
-                if self.fish_tank.full():
-                    print(f'{self.name}, {worm}: садок полон !!!', flush=True)
-                self.fish_tank.put(fish)
-                print(f'{self.name}, {worm}: наконец-то положил {fish} в садок', flush=True)
+                if self.catcher.full():
+                    print(f'{self.name}, {worm}: приемщик полон !!!', flush=True)
+                self.catcher.put(fish)
+                print(f'{self.name}, {worm}: наконец-то отдал {fish} приемщику', flush=True)
+
+
+class Boat(Thread):
+    def __init__(self, worms_per_fisher=10, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fishers = []
+        self.worms_per_fisher = worms_per_fisher
+        self.catcher = queue.Queue(maxsize=2)
+        self.fish_tank = defaultdict(int)
+
+    def add_fisher(self, name):
+        fisher = Fisher(name=name, worms=self.worms_per_fisher, catcher=self.catcher)
+        self.fishers.append(fisher)
+
+    def run(self):
+        print('Лодка вышла в море...', flush=True)
+        for fisher in self.fishers:
+            fisher.start()
+        while True:
+            try:
+                fish = self.catcher.get(timeout=1)
+                print(f'Приемщик принял {fish} и положил в садок', flush=True)
+                self.fish_tank[fish] += 1
+            except queue.Empty:
+                print(f'Приемщику нет рыбы в течении 1 секунды', flush=True)
+                if not any(fisher.is_alive() for fisher in self.fishers):
+                    break
+        for fisher in self.fishers:
+            fisher.join()
+        print(f'Лодка возвращается домой {self.fish_tank}', flush=True)
+
+
+boat = Boat(worms_per_fisher=10)
+
+humans = ['Васек', 'Колян', 'Петрович', 'Хмурый', 'Клава']
+for name in humans:
+    boat.add_fisher(name=name)
+
+boat.start()
+boat.join()
+
+print(f'Лодка привезла {boat.fish_tank}')
