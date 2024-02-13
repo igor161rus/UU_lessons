bc97fa7be246a9c86f499464ca9dc48ad4b6c0e5
igor161rus <dranyi.igor@gmail.com>
2024-02-13 12:13:26 +0300

    Домашнее задание по теме "Очереди для обмена данными между потоками."

--- lesson_010_1/01_hw_v3.py
+++ lesson_010_1/01_hw_v3.py
@@ -62,7 +62,8 @@ import random
 from datetime import time
 from threading import Thread
 from time import sleep
-import queue
+# import queue
+from queue import Empty, Queue
 
 
 class Table:
@@ -77,39 +78,37 @@ class Cafe(Thread):
 
     def __init__(self, tables, *args, **kwargs):
         super(Cafe, self).__init__(*args, **kwargs)
-        self.table = queue.Queue()
+        self.table = Queue()
         self.tables = tables
-        self.customer = queue.Queue()
+        self.customer = Queue()
         for i, table in enumerate(self.tables):
             self.table.put(i + 1)
 
     def customer_arrival(self):
         """моделирует приход посетителя(каждую секунду)."""
-        for i in range(1, 10):
-            # print(f'Посетитель номер {customer} прибыл', flush=True)
+        for i in range(1, 21):
             customer = Customer(customer=i, table=0)
             self.customer.put(customer)
         while not self.customer.empty():
-            # try:
             customer = self.customer.get()
             print(f'Посетитель номер {customer.customer} прибыл', flush=True)
             self.serve_customer(customer=customer)
             sleep(1)
-            # except queue.Empty:
-            #     print(f'Очередь пуста')
 
     def serve_customer(self, customer):
         """моделирует обслуживание посетителя. Проверяет наличие свободных столов,"""
-        # if not self.table.empty():
-        try:
+        if not self.table.empty():
             table = self.table.get()
-            # customer = Customer(customer=self.customer.get(), table=table)
             customer.table = table
             customer.start()
-            self.table.task_done()
-            # customer.join()
-        except queue.Empty:
-            print(f'Посетитель номер {self.customer} ожидает свободный стол')
+        else:
+            print(f'Посетитель номер {customer.customer} ожидает свободный стол')
+            while self.table.empty():
+                sleep(1)
+            if not self.table.empty():
+                table = self.table.get()
+                customer.table = table
+                customer.start()
 
 
 class Customer(Thread):
@@ -124,6 +123,7 @@ class Customer(Thread):
         print(f'Посетитель номер {self.customer} сел за стол {self.table}', flush=True)
         sleep(5)
         print(f'Посетитель номер {self.customer} освободил стол {self.table}', flush=True)
+        cafe.table.task_done()
         cafe.table.put(self.table)
         cafe.customer.task_done()
         cafe.customer.join()
@@ -144,3 +144,74 @@ customer_arrival_thread.start()
 
 # Ожидаем завершения работы прибытия посетителей
 customer_arrival_thread.join()
+
+# Результат:
+# D:\Python\Python39\python.exe D:\Python\Projects\UU\lessons\lesson_010_1\01_hw_v3.py
+# Посетитель номер 1 прибыл
+# Посетитель номер 1 сел за стол 1
+# Посетитель номер 2 прибыл
+# Посетитель номер 2 сел за стол 2
+# Посетитель номер 3 прибыл
+# Посетитель номер 3 сел за стол 3
+# Посетитель номер 4 прибыл
+# Посетитель номер 4 ожидает свободный стол
+# Посетитель номер 1 освободил стол 1
+# Посетитель номер 4 сел за стол 1
+# Посетитель номер 2 освободил стол 2
+# Посетитель номер 5 прибыл
+# Посетитель номер 5 сел за стол 2
+# Посетитель номер 3 освободил стол 3
+# Посетитель номер 6 прибыл
+# Посетитель номер 6 сел за стол 3
+# Посетитель номер 7 прибыл
+# Посетитель номер 7 ожидает свободный стол
+# Посетитель номер 4 освободил стол 1
+# Посетитель номер 7 сел за стол 1
+# Посетитель номер 5 освободил стол 2
+# Посетитель номер 8 прибыл
+# Посетитель номер 8 сел за стол 2
+# Посетитель номер 6 освободил стол 3
+# Посетитель номер 9 прибыл
+# Посетитель номер 9 сел за стол 3
+# Посетитель номер 10 прибыл
+# Посетитель номер 10 ожидает свободный стол
+# Посетитель номер 7 освободил стол 1
+# Посетитель номер 10 сел за стол 1
+# Посетитель номер 8 освободил стол 2
+# Посетитель номер 11 прибыл
+# Посетитель номер 11 сел за стол 2
+# Посетитель номер 9 освободил стол 3
+# Посетитель номер 12 прибыл
+# Посетитель номер 12 сел за стол 3
+# Посетитель номер 13 прибыл
+# Посетитель номер 13 ожидает свободный стол
+# Посетитель номер 10 освободил стол 1
+# Посетитель номер 13 сел за стол 1
+# Посетитель номер 11 освободил стол 2
+# Посетитель номер 14 прибыл
+# Посетитель номер 14 сел за стол 2
+# Посетитель номер 12 освободил стол 3
+# Посетитель номер 15 прибыл
+# Посетитель номер 15 сел за стол 3
+# Посетитель номер 16 прибыл
+# Посетитель номер 16 ожидает свободный стол
+# Посетитель номер 13 освободил стол 1
+# Посетитель номер 16 сел за стол 1
+# Посетитель номер 14 освободил стол 2
+# Посетитель номер 17 прибыл
+# Посетитель номер 17 сел за стол 2
+# Посетитель номер 15 освободил стол 3
+# Посетитель номер 18 прибыл
+# Посетитель номер 18 сел за стол 3
+# Посетитель номер 19 прибыл
+# Посетитель номер 19 ожидает свободный стол
+# Посетитель номер 16 освободил стол 1
+# Посетитель номер 19 сел за стол 1
+# Посетитель номер 17 освободил стол 2
+# Посетитель номер 20 прибыл
+# Посетитель номер 20 сел за стол 2
+# Посетитель номер 18 освободил стол 3
+# Посетитель номер 19 освободил стол 1
+# Посетитель номер 20 освободил стол 2
+#
+# Process finished with exit code 0
