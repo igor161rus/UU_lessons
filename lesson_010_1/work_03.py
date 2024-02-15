dcf3ceb281f513b6d7c2aaeee11468365f900918
igor161rus <dranyi.igor@gmail.com>
2024-02-15 10:04:04 +0300

    Практика однопоточная. Часть 1.

--- lesson_010_1/work_03.py
+++ lesson_010_1/work_03.py
@@ -7,6 +7,11 @@ from html.parser import HTMLParser
 sites = [
     # 'https://www.kinopoisk.ru/',
     'https://www.fl.ru',
+    'https://www.weblancer.net/',
+    'https://www.freelancejob.ru/',
+    # 'https://kwork.ru',
+    'https://work-zilla.com/',
+    # 'https://iklife.ru/udalennaya-rabota-i-frilans/poisk-raboty/vse-samye-luchshie-sajty-i-birzhi-v-internete.html',
     # 'https://www.youtube.com/',
     # 'https://www.urban-university.ru/',
     # 'https://www.ivi.ru/',
@@ -22,7 +27,8 @@ class LinkExtractor(HTMLParser):
         self.links = []
 
     def handle_starttag(self, tag, attrs):
-        if tag != 'link':
+        # if tag != 'link':
+        if tag not in ('link', 'script',):
             return
         # print("Start tag:", tag)
         # for attr in attrs:
@@ -37,9 +43,9 @@ class LinkExtractor(HTMLParser):
 
 for url in sites:
     print(f'Go {url}...')
-    res = urlopen(url)
-    html_data = res.read()
-    html_data = html_data.decode('utf8')
+    res = requests.get(url)
+    html_data = res.text
+    # html_data = html_data.decode('utf8')
     total_bytes = len(html_data)
     extractor = LinkExtractor()
     extractor.feed(html_data)
@@ -50,6 +56,7 @@ for url in sites:
             res = urlopen(link)
         except Exception as exc:
             print(exc)
-        extra_data = res.read()
+        # extra_data = res.read()
+        extra_data = res.text
         total_bytes += len(extra_data)
     print(f'For url {url} need download {total_bytes} bytes')
