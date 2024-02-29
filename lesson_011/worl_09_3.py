10a445d2bf764e7ba5db7abb6f2a0bd1b69372d0
igor161rus <dranyi.igor@gmail.com>
2024-02-29 13:18:35 +0300

    Пример использования библиотек ч. 3.

--- lesson_011/worl_09_3.py
+++ lesson_011/worl_09_3.py
@@ -12,21 +12,27 @@ class PostCardMaker:
         else:
             self.font_path = font_path
 
-    def make(self):
+    def make(self, resize=False, out_path=None):
         im = Image.open("postcard.jpg")
-
         w, h = im.size
-        resized_im = im.resize((w // 2, h // 2))
-
-        draw = ImageDraw.Draw(resized_im)
-
-        font = ImageFont.truetype(self.font_path, 16)
-
+        font_size = 32
+        if resize:
+            im = im.resize((w // 2, h // 2))
+            font_size = font_size // 2
+        draw = ImageDraw.Draw(im)
+        font = ImageFont.truetype(self.font_path, font_size)
+        y = im.size[1] - 340 - font.size
+        x = im.size[0] // 20
         message = f'Привет, {self.name}!'
-        draw.text((15, 110), message, font=font, fill=ImageColor.colormap['red'])
+        draw.text((x, y), message, font=font, fill=ImageColor.colormap['red'])
 
-        resized_im.show()
-        # resized_im.save('postcard_edit.jpg')
+        y = im.size[1] - 305 - font.size
+        message = f'С Праздником!!!'
+        draw.text((x, y), message, font=font, fill=ImageColor.colormap['red'])
+        im.show()
+        out_path = out_path if out_path else 'postcard_edit.jpg'
+        im.save(out_path)
+        print(f'Post card saved az {out_path}')
 
 
 if __name__ == '__main__':
