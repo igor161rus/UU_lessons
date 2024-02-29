d08e1662564512051ce29790654267392339569a
Igor <dranyi.igor@gmail.com>
2024-02-29 08:35:15 +0300

    Домашнее задание по теме "Многопроцессное программирование"

--- lesson_010_1/02_hw_v3.py
+++ lesson_010_1/02_hw_v3.py
@@ -24,36 +24,26 @@ from multiprocessing import Process, Queue, Manager
 
 
 class WarehouseManager(Process):
-    def __init__(self, request, *args, **kwargs):
+    def __init__(self, request, data, *args, **kwargs):
         super(WarehouseManager, self).__init__(*args, **kwargs)
-        self.data = Manager().dict()
-        self.works = []
         self.request = request
-        # self.data_queue = Queue()
+        self.data = data
 
     def run(self):
-        print(self.request)
         print(f'{self.request} parent process:', os.getppid())
         print(f'{self.request} process id:', os.getpid())
-        # self.data_queue.put(self.data)
+        self.process_request()
 
-        # while not self.data_queue.empty():
-        #     data = self.data_queue.get()
-        #     print(data)
-
-    def process_request(self, request):
-        print(1 + 1)
-        print(f'{request} parent process:', os.getppid())
-        print(f'{request} process id:', os.getpid())
-        # if request[0] not in self.data:
-        # self.data[request[0]] = request[2]
-        # elif 'receipt' in request:
-        #     self.data[request[0]] += request[2]
-        # elif 'shipment' in request:
-        #     self.data[request[0]] -= request[2]
+    def process_request(self):
+        if self.request[0] not in self.data:
+            self.data[self.request[0]] = self.request[2]
+        elif 'receipt' in self.request:
+            self.data[self.request[0]] += self.request[2]
+        elif 'shipment' in self.request:
+            self.data[self.request[0]] -= self.request[2]
 
 
-def main():
+if __name__ == '__main__':
     requests = [
         ("product1", "receipt", 100),
         ("product2", "receipt", 150),
@@ -61,17 +51,32 @@ def main():
         ("product3", "receipt", 200),
         ("product2", "shipment", 50)
     ]
-    manager = WarehouseManager(requests)
+    data = Manager().dict()
+    manager = WarehouseManager(requests, data)
     works = []
     for request in requests:
-        req_work = WarehouseManager(request)
+        req_work = WarehouseManager(request, data)
         works.append(req_work)
     for work in works:
         work.start()
     for work in works:
         work.join()
-    print(manager.data)
+    print('*' * 20, '\n', manager.data)
 
 
-if __name__ == '__main__':
-    main()
+# Результат
+# C:\Users\user\AppData\Local\Programs\Python\Python310\python.exe C:\pytnon\projects\UU_lessons\lesson_010_1\02_hw_v3.py
+# ('product1', 'receipt', 100) parent process: 5416
+# ('product1', 'receipt', 100) process id: 3184
+# ('product2', 'receipt', 150) parent process: 5416
+# ('product2', 'receipt', 150) process id: 6596
+# ('product1', 'shipment', 30) parent process: 5416
+# ('product1', 'shipment', 30) process id: 16968
+# ('product3', 'receipt', 200) parent process: 5416
+# ('product3', 'receipt', 200) process id: 19992
+# ('product2', 'shipment', 50) parent process: 5416
+# ('product2', 'shipment', 50) process id: 16636
+# ********************
+#  {'product1': 70, 'product2': 100, 'product3': 200}
+#
+# Process finished with exit code 0
