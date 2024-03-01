8ab93f670775f33a0160f67b9444749f827a4449
Igor <dranyi.igor@gmail.com>
2024-03-01 09:23:33 +0300

    Введение в тестирование.

---
+++ lesson_012/work_03.py
@@ -0,0 +1,41 @@
+def my_sort(slist):
+    """
+    Функция сортировки списков
+
+    >>> my_sort([3,2,1])
+    [1, 2, 3]
+    """
+    was_swap = True
+    while was_swap:
+        was_swap = False
+        for i in range(len(slist) - 1):
+            if slist[i] > slist[i + 1]:
+                slist[i], slist[i + 1] = slist[i + 1], slist[i]
+                was_swap = True
+    return slist
+
+
+# print(my_sort([3, 4, 2, 8, 1, 6, 4]))
+# print(my_sort([3, 4, 5]))
+# print(my_sort([3, 2, 1]))
+# print(my_sort([]))
+# print(my_sort([9, 3, -7, 2]))
+#
+#
+# if my_sort([3, 4, 2, 8, 1, 6, 4]) != [1, 2, 3, 4, 4, 6, 8]:
+#     print('Error')
+# if my_sort([3, 4, 5]) != [3, 4, 5]:
+#     print('Error!')
+# if my_sort([3, 2, 1]) != [1, 2, 3]:
+#     print('Error')
+# if my_sort([]):
+#     print('Error!')
+# if my_sort([9, 3, -7, 2]) != [-7, 2, 3, 9]:
+#     print('Error!')
+
+# assert my_sort([3, 4, 2, 8, 1, 6, 4]) == [1, 2, 3, 4, 4, 6, 8], 'что-то не работает'
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
