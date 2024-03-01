79943c652e91394d8e143006b88e5096e1ffb4ee
igor161rus <dranyi.igor@gmail.com>
2024-03-01 11:18:28 +0300

    Пишем тесты.

--- lesson_012/work_04.py
+++ lesson_012/work_04.py
@@ -1,4 +1,4 @@
-def my_sort(slist):
+def my_sort_v1(slist):
     was_swap = True
     while was_swap:
         was_swap = False
@@ -7,3 +7,20 @@ def my_sort(slist):
                 slist[i], slist[i + 1] = slist[i + 1], slist[i]
                 was_swap = True
     return slist
+
+
+def my_sort(slist):
+    if len(slist) <= 1:
+        return slist
+    pivot = slist[0]
+    less_then = []
+    more_then = []
+    equal = []
+    for elem in slist:
+        if elem > pivot:
+            more_then.append(elem)
+        elif elem < pivot:
+            less_then.append(elem)
+        else:
+            equal.append(elem)
+    return my_sort(less_then) + equal + my_sort(more_then)
