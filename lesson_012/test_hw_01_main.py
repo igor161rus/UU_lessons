f447a133e4f69243b34a27d7dbd739cdeb0cc09c
igor161rus <dranyi.igor@gmail.com>
2024-03-02 19:41:29 +0300

    Домашнее задание по теме "Тестирование"

--- lesson_012/test_hw_01_main.py
+++ lesson_012/test_hw_01_main.py
@@ -1,6 +1,18 @@
 import unittest
-from  import Student
+from hw_01_main import Student
 
-class StudentTest(unittest):
+
+class StudentTest(unittest.TestCase):
+
+    def setUp(self):
+        self.student = Student('test')
+
     def test_walk(self):
-        self.student = Student()
+        for _ in range(10):
+            self.student.walk()
+        self.assertEqual(self.student.distance, 500,
+                         'Дистанции не равны [дистанция человека({})] != 500'.format(self.student.name))
+
+
+if __name__ == '__main__':
+    unittest.main()
