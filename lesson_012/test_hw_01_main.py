d7421bda056db1782c8273205885d85e7bfee86c
igor161rus <dranyi.igor@gmail.com>
2024-03-03 18:57:34 +0300

    Домашнее задание по теме "Тестирование"

--- lesson_012/test_hw_01_main.py
+++ lesson_012/test_hw_01_main.py
@@ -7,16 +7,17 @@ class StudentTest(unittest.TestCase):
     def setUp(self):
         self.student = Student('Вася')
         self.dict_test = {
-            'test_walk_equal': 'Дистанции не равны [дистанция человека({})] != 500',
-            'test_walk_greater': 'Дистанция {} меньше 100',
-            'test_run_equal': 'Дистанции не равны [дистанция человека({})] != 1000'
+            'test_walk_equal': 'Дистанции не равны [дистанция человека({})] != 50',
+            'test_walk_greater': 'Дистанция {} больше, 5',
+            'test_run_equal': 'Дистанции не равны [дистанция человека({})] != 1000',
+            'test_walk_less': 'Дистанция {} меньше 100',
         }
 
     def test_walk_equal(self):
         message = self.dict_test[self.test_walk_equal.__name__]
         for _ in range(10):
             self.student.walk()
-        self.assertEqual(self.student.distance, 500, message.format(self.student.name))
+        self.assertEqual(self.student.distance, 50, message.format(self.student.name))
 
     def test_walk_greater(self):
         message = self.dict_test[self.test_walk_greater.__name__]
@@ -24,11 +25,17 @@ class StudentTest(unittest.TestCase):
             self.student.walk()
         self.assertGreater(self.student.distance, 5, message.format(self.student.name))
 
+    def test_walk_less(self):
+        message = self.dict_test[self.test_walk_less.__name__]
+        for _ in range(10):
+            self.student.walk()
+        self.assertLess(self.student.distance, 100, message.format(self.student.name))
+
     def test_run_equal(self):
         message = self.dict_test[self.test_run_equal.__name__]
         for _ in range(10):
             self.student.run()
-        self.assertEqual(self.student.distance, 100, message.format(self.student.name))
+        self.assertEqual(self.student.distance, 10, message.format(self.student.name))
 
 
 if __name__ == '__main__':
