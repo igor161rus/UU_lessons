81d06907afd0dbbf57308c761751c8bb009e2f6f
igor161rus <dranyi.igor@gmail.com>
2024-03-03 16:18:29 +0300

    Домашнее задание по теме "Тестирование"

--- lesson_012/test_hw_01_main.py
+++ lesson_012/test_hw_01_main.py
@@ -5,19 +5,30 @@ from hw_01_main import Student
 class StudentTest(unittest.TestCase):
 
     def setUp(self):
-        self.student = Student('test')
+        self.student = Student('Вася')
+        self.dict_test = {
+            'test_walk_equal': 'Дистанции не равны [дистанция человека({})] != 500',
+            'test_walk_greater': 'Дистанция {} меньше 100',
+            'test_run_equal': 'Дистанции не равны [дистанция человека({})] != 1000'
+        }
 
-    def test_walk(self):
+    def test_walk_equal(self):
+        message = self.dict_test[self.test_walk_equal.__name__]
         for _ in range(10):
             self.student.walk()
-        self.assertEqual(self.student.distance, 50,
-                         'Дистанции не равны [дистанция человека({})] != 500'.format(self.student.name))
+        self.assertEqual(self.student.distance, 500, message.format(self.student.name))
 
-    def test_run(self):
+    def test_walk_greater(self):
+        message = self.dict_test[self.test_walk_greater.__name__]
+        for _ in range(10):
+            self.student.walk()
+        self.assertGreater(self.student.distance, 5, message.format(self.student.name))
+
+    def test_run_equal(self):
+        message = self.dict_test[self.test_run_equal.__name__]
         for _ in range(10):
             self.student.run()
-        self.assertEqual(self.student.distance, 100,
-                         'Дистанции не равны [дистанция человека({})] != 1000'.format(self.student.name))
+        self.assertEqual(self.student.distance, 100, message.format(self.student.name))
 
 
 if __name__ == '__main__':
