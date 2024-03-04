334f96e37bccb3a209967f61392e5d56e50133f8
igor161rus <dranyi.igor@gmail.com>
2024-03-04 11:29:25 +0300

    Тестирование с объектами-пустышками.

---
+++ lesson_012/hw_03_01.py
@@ -0,0 +1,23 @@
+from unittest.mock import Mock
+
+fake_1 = Mock()
+print(fake_1)
+print(fake_1())
+
+# print(fake_1(1, 2, 3, test=42))
+# print(fake_1.called)
+# print(fake_1(test=42))
+# print(fake_1.call_count)
+# print(fake_1.call_args)
+# fake_1.assert_called_with(test=42)
+#
+# fake_2 = Mock(return_value=42)
+#
+# fake_3 = Mock()
+# print(fake_3.nonexistent_attr)
+# fake_3.any_attr = 27
+# print(fake_3.any_attr)
+#
+# fake_4 = Mock(return_value=fake_3)
+# result = fake_4()
+# print(result.any_attr)
--- lesson_012/test_hw_03.py
+++ lesson_012/test_hw_03.py
@@ -1,6 +1,7 @@
 import unittest
 
 from hw_03 import ExternalResourceGetter
+from lesson_012 import hw_03
 
 _test_data = """
 1234567
@@ -22,8 +23,9 @@ def fake_get_result(*args, **kwargs):
 class ExternalResourceGetterTest(unittest.TestCase):
     def test_normal(self):
         getter = ExternalResourceGetter(url='https://www.python.org')
+        hw_03.requests.get = fake_get_result
         result = getter.run()
-        self.assertEqual(result, 535)
+        self.assertEqual(result, 9)
 
 
 if __name__ == '__main__':
