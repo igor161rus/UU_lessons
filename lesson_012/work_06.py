ab3a6ad099a1590b499aeb7f9a30c24f3cd893f9
igor161rus <dranyi.igor@gmail.com>
2024-03-04 09:50:05 +0300

    Введение в логирование.

---
+++ lesson_012/work_06.py
@@ -0,0 +1,25 @@
+import logging
+
+def prime_numbers_generator(n):
+    prime_numbers = []
+    for number in range(2, n + 1):
+        logging.debug(f'number {number}')
+        for prime in prime_numbers:
+            if number % prime == 0:
+                logging.debug(f'делится на {prime}')
+                break
+        else:
+            logging.debug(f'найдено новое простое {number}')
+            prime_numbers.append(number)
+            yield number
+
+logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.INFO)
+
+for prime in prime_numbers_generator(100):
+    logging.info(f'Простое из генераторов {prime}')
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    handlers=[logging.FileHandler('primes.log', 'w', 'utf-8')]
+)
