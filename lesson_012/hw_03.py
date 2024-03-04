ad289657bb71e11cf323deff9243a2f63e4170ce
igor161rus <dranyi.igor@gmail.com>
2024-03-04 11:04:43 +0300

    Тестирование с объектами-пустышками.

---
+++ lesson_012/hw_03.py
@@ -0,0 +1,26 @@
+import requests
+
+class ExternalResourceGetter:
+    def __init__(self, url):
+        self.url = url
+        self.data = None
+
+    def run(self):
+        self.data = self.get_data()
+        result = self.proceed_data()
+        return result
+
+    def get_data(self):
+        response = requests.get(self.url)
+        return response.text
+
+    def proceed_data(self):
+        max_length = max([len(line) for line in self.data.split('\n')])
+        return max_length
+
+
+if __name__ == '__main__':
+    getter = ExternalResourceGetter(url='https://www.jetbrains.com/pycharm/')
+
+    data = getter.run()
+    print(data)
