e1acbbeef6448a756ad10f69475a78cfd9d1ed51
igor161rus <dranyi.igor@gmail.com>
2024-03-04 11:13:33 +0300

    Тестирование с объектами-пустышками.

---
+++ lesson_012/test_hw_03.py
@@ -0,0 +1,30 @@
+import unittest
+
+from hw_03 import ExternalResourceGetter
+
+_test_data = """
+1234567
+12345
+123456789
+12
+"""
+
+
+class FakeResult:
+    def __init__(self):
+        self.text = _test_data
+
+
+def fake_get_result(*args, **kwargs):
+    return FakeResult()
+
+
+class ExternalResourceGetterTest(unittest.TestCase):
+    def test_normal(self):
+        getter = ExternalResourceGetter(url='https://www.python.org')
+        result = getter.run()
+        self.assertEqual(result, 9)
+
+
+if __name__ == '__main__':
+    unittest.main()
