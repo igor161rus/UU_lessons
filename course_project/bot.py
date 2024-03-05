bb10cbc5333aaec3d49c9379d72b10837d501970
igor161rus <dranyi.igor@gmail.com>
2024-03-05 09:38:38 +0300

    Курсовой проект. Начало.

--- course_project/bot.py
+++ course_project/bot.py
@@ -1,8 +1,9 @@
 import random
 
 import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from settings import club, token
+
 
 class Bot:
     def __init__(self, club, token):
@@ -14,7 +15,6 @@ class Bot:
 
         self.api = self.vk.get_api()
 
-
     def run(self):
         for event in self.long_poller.listen():
             print('получено событие')
@@ -24,13 +24,14 @@ class Bot:
                 print(exc)
 
     def on_event(self, event):
-        if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
+        if event.type == VkBotEventType.MESSAGE_NEW:
             print(event.message.text)
-            self.api.message.send(message=event.object.text,
-                                  random_id=random.randint(0, 2 ** 20),
-                                  peer_id=event.object.peer_id )
+            self.api.messages.send(message=event.message.text,
+                                   random_id=0,
+                                   peer_id=event.message.peer_id)
         else:
             print('hgjhg ghghfgfdf hjjk', event.type)
+
 
 if __name__ == '__main__':
     bot = Bot(club=club, token=token)
