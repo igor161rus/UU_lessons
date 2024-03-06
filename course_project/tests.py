09b780ec5d96acd83142b3697ab57ffceb002157
igor161rus <dranyi.igor@gmail.com>
2024-03-06 11:10:47 +0300

    Курсовой проект: тест на функцию "run".

--- course_project/tests.py
+++ course_project/tests.py
@@ -1,6 +1,22 @@
 from unittest import TestCase
+from unittest.mock import patch, Mock
+
+from bot import Bot
 
 
 class Test1(TestCase):
     def test_1(self):
-        assert True
+        count = 5
+        events = [{}] * count
+        long_poller_mock = Mock(return_value=events)
+        long_poller_listen_mock = Mock()
+        long_poller_listen_mock.listen = long_poller_mock
+        with patch('bot.vk_api.VkApi'):
+            with patch('bot.VkBotLongPoll', return_value=long_poller_listen_mock):
+                bot = Bot(club='', token='')
+                bot.on_event = Mock()
+                bot.run()
+
+                bot.on_event.assert_called()
+                bot.on_event.assert_any_call({})
+                bot.on_event.call_count == count
