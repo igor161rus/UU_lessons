52ae43f24520e3ae386e7369e4926372ee10cdfb
igor161rus <dranyi.igor@gmail.com>
2024-03-08 17:01:04 +0300

    Вебинар по модулю "Поддержка цикла разработки ч. 2"

--- lesson_013/webinar/tests/test_module.py
+++ lesson_013/webinar/tests/test_module.py
@@ -17,13 +17,16 @@ class PlayerTest(unittest.TestCase):
                     exit()
                 if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
                     status = False
+
             screen.fill('black')
+
             fps.tick(60)
             player.update(screen)
             pg.display.update()
 
         widht_result = player.rect.left >= 0 and player.rect.right <= screen.get_width()
         height_result = player.rect.top >= 0 and player.rect.bottom <= screen.get_height()
+
         return self.assertTrue(widht_result and height_result,
                                f'Выход за границу экрана: {player.rect.x}, {player.rect.y}')
 
