9dfde26499e2d0eff3e27a016ee739920a679129
igor161rus <dranyi.igor@gmail.com>
2024-03-12 11:48:47 +0300

    Практическое задание "Анализатор прайс-листов."

---
+++ practice_01/src/project.py
@@ -0,0 +1,76 @@
+import os, fnmatch
+import json
+import pandas
+
+
+class PriceMachine():
+
+    def __init__(self):
+        self.data = []
+        self.result = ''
+        self.name_length = 0
+
+    def load_prices(self, file_path=''):
+        """
+            Сканирует указанный каталог. Ищет файлы со словом price в названии.
+            В файле ищет столбцы с названием товара, ценой и весом.
+            Допустимые названия для столбца с товаром:
+                товар
+                название
+                наименование
+                продукт
+
+            Допустимые названия для столбца с ценой:
+                розница
+                цена
+
+            Допустимые названия для столбца с весом (в кг.)
+                вес
+                масса
+                фасовка
+        """
+        pattern = '*price*'
+        list_files = os.listdir(file_path)
+        files = [entry for entry in list_files if fnmatch.fnmatch(entry, pattern)]
+        for file in files:
+            with open(file_path + '/' + file, 'r', encoding='1251') as f:
+                data = json.load(f)
+                self.data.append(data)
+                print(self.data)
+
+    def _search_product_price_weight(self, headers):
+        """
+            Возвращает номера столбцов
+        """
+
+    def export_to_html(self, fname='output.html'):
+        result = '''
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Позиции продуктов</title>
+        </head>
+        <body>
+            <table>
+                <tr>
+                    <th>Номер</th>
+                    <th>Название</th>
+                    <th>Цена</th>
+                    <th>Фасовка</th>
+                    <th>Файл</th>
+                    <th>Цена за кг.</th>
+                </tr>
+        '''
+
+    def find_text(self, text):
+        pass
+
+
+pm = PriceMachine()
+print(pm.load_prices('D:/Python/Projects/UU/lessons/practice_01/data'))
+
+'''
+    Логика работы программы
+'''
+print('the end')
+# print(pm.export_to_html())
