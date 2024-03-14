5ce152fa5413b374f82fd838cd4bf201914dd792
igor161rus <dranyi.igor@gmail.com>
2024-03-14 15:14:48 +0300

    Домашнее задание по теме "Регулярные выражения"

--- lesson_014/hw_02.py
+++ lesson_014/hw_02.py
@@ -29,3 +29,17 @@
 # Учтите что 'http' это подстрока строки 'https'.
 #
 # Файл с исходным кодом прикрепите к домашнему заданию.
+
+
+def extract_image_links(html_text):
+    pass
+
+
+sample_html = "<img src='https://example.com/image1.jpg'> <img src='http://example.com/image2.png'> <img src='https://example.com/image3.gif'>"
+
+image_links = extract_image_links(sample_html)
+if image_links:
+    for image_link in image_links:
+        print(image_link)
+else:
+    print("Нет ссылок с картинками в HTML тексте.")
