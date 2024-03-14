602d447489f1dbc0bb1956d405a9882231339cef
Igor <dranyi.igor@gmail.com>
2024-03-14 16:10:45 +0300

    Домашнее задание по теме "Регулярные выражения"

--- lesson_014/hw_02.py
+++ lesson_014/hw_02.py
@@ -30,16 +30,24 @@
 #
 # Файл с исходным кодом прикрепите к домашнему заданию.
 
+import re
+
 
 def extract_image_links(html_text):
-    pass
+    # urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', html_text)
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', html_text)
+    print(urls)
+    # if match:
+    #     print(match.group(1))
 
 
 sample_html = "<img src='https://example.com/image1.jpg'> <img src='http://example.com/image2.png'> <img src='https://example.com/image3.gif'>"
 
-image_links = extract_image_links(sample_html)
-if image_links:
-    for image_link in image_links:
-        print(image_link)
-else:
-    print("Нет ссылок с картинками в HTML тексте.")
+extract_image_links(sample_html)
+
+# image_links = extract_image_links(sample_html)
+# if image_links:
+#     for image_link in image_links:
+#         print(image_link)
+# else:
+#     print("Нет ссылок с картинками в HTML тексте.")
