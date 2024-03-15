f30541f147d8f72c24654bfaa997bf460d2d8ce2
igor161rus <dranyi.igor@gmail.com>
2024-03-15 14:21:12 +0300

    Домашнее задание по теме "Регулярные выражения"

--- lesson_014/hw_02.py
+++ lesson_014/hw_02.py
@@ -50,19 +50,40 @@ def extract_image_links(html_text):
     return image_links
 
 
+def extract_image_links_v2(html_text):
+    """Версия в которой сразу отбираются нужные расширения"""
+    pattern = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+])(?:[^'>])+(?:jpg|png|gif|jpeg)"
+    urls = re.findall(pattern, html_text)
+    return urls
+
+
 sample_html = ("<img src='https://example.com/image1.jpg'> <img src='http://example.com/image2.png'> "
                "<img src='https://example.com/image3.gif'> <src='https://example.com/image3.py'>"
-               "<img src='http://example.com/image4.jpeg'> <img src='https://example.com/image4.zip'>")
+               "<img src='http://example.com/image4.jpeg'> <img src='https://example.com/image4.zip'>"
+               "<img src='https://example.com/image3.txt'> <src='https://example.com/image3.jpe'>")
+
+
 # sample_html = ("<img src='https://example.com/image4.zip'>")
 
-extract_image_links(sample_html)
+def print_result(list_image_links):
+    if list_image_links:
+        for image_link in list_image_links:
+            print(image_link)
+    else:
+        print("Нет ссылок с картинками в HTML тексте.")
 
-image_links = extract_image_links(sample_html)
-if image_links:
-    for image_link in image_links:
-        print(image_link)
-else:
-    print("Нет ссылок с картинками в HTML тексте.")
+
+print_result(extract_image_links(sample_html))
+print('\n', '*' * 10, 'v2', '*' * 10)
+print_result(extract_image_links_v2(sample_html))
+
+# image_links = extract_image_links(sample_html)
+# if image_links:
+#     for image_link in image_links:
+#         print(image_link)
+# else:
+#     print("Нет ссылок с картинками в HTML тексте.")
+
 
 # Результат
 # D:\Python\Python312\python.exe D:\Python\Projects\UU\lessons\lesson_014\hw_02.py
@@ -71,9 +92,19 @@ else:
 # https://example.com/image3.gif
 # http://example.com/image4.jpeg
 #
+#  ********** v2 **********
+# https://example.com/image1.jpg
+# http://example.com/image2.png
+# https://example.com/image3.gif
+# http://example.com/image4.jpeg
+#
 # Process finished with exit code 0
 
+
 # D:\Python\Python312\python.exe D:\Python\Projects\UU\lessons\lesson_014\hw_02.py
+# Нет ссылок с картинками в HTML тексте.
+#
+#  ********** v2 **********
 # Нет ссылок с картинками в HTML тексте.
 #
 # Process finished with exit code 0
