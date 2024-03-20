e52e958e8ad12edef203b5008b63f6c6867e323b
igor161rus <dranyi.igor@gmail.com>
2024-03-20 14:30:01 +0300

    Практика. Часть 1.

--- lesson_014/work_08_practice/work_08.py
+++ lesson_014/work_08_practice/work_08.py
@@ -1,7 +1,10 @@
+import csv
 import json
 import re
+from collections import defaultdict
 from datetime import datetime
-from decimal import Decimal
+from decimal import Decimal, ROUND_HALF_EVEN
+from pprint import pprint
 
 re_data = r'jbd(\d{6})'
 re_city = r'jbc(\w+)jbc'
@@ -56,3 +59,55 @@ for key, message in data.items():
 print(result)
 result = sorted(result, key=lambda x: x['date'])
 print(result)
+
+result_formatted = [{
+    'date': record['date'].strftime('%d.%m.%Y'),
+    'city': record['city'],
+    'expanses': str(record['expanses'].quantize(Decimal('1.00'), ROUND_HALF_EVEN))
+
+}
+    for record in result
+]
+pprint(result_formatted)
+
+with open('data/PBDetail.csv', 'w', encoding='utf-8') as f:
+    writer = csv.DictWriter(f, fieldnames=['date', 'city', 'expanses'])
+    writer.writeheader()
+    writer.writerows(result_formatted)
+
+result_aggregated_temp = defaultdict(lambda: {'cities': set(), 'expanses_sum': Decimal(0),
+                                              'month': '', 'date_for_sort': None})
+
+for record in result:
+    month_datetime = datetime(year=record['date'].year, month=record['date'].month, day=1)
+    month = month_datetime.strftime('%m-%Y')
+    result_aggregated_temp[month]['cities'].add(record['city'])
+    result_aggregated_temp[month]['expanses_sum'] += record['expanses']
+    result_aggregated_temp[month]['month'] = month
+    result_aggregated_temp[month]['date_for_sort'] = month_datetime
+
+pprint(result_aggregated_temp)
+
+result_aggregated = sorted(result_aggregated_temp.values(), key=lambda x: x['date_for_sort'])
+
+result_aggregated_formated = [{
+    'month': record['month'],
+    'cities': ', '.join(record['cities']),
+    'expanses_sum': str(record['expanses_sum'].quantize(Decimal('1.00'), ROUND_HALF_EVEN))
+}
+    for record in result_aggregated
+]
+pprint(result_aggregated_formated)
+
+with open('data/PBMonth.csv', 'w', encoding='utf-8') as f:
+    writer = csv.DictWriter(f, fieldnames=['month', 'cities', 'expanses_sum'])
+    writer.writeheader()
+    writer.writerows(result_aggregated_formated)
+
+
+
+
+
+
+
+
