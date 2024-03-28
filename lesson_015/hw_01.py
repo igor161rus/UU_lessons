df77a49d77c4d45c3c5d90d32bb6eb21487f12b1
igor <dranyi.igor@gmail.com>
2024-03-29 02:17:44 +0300

    Домашнее задание по теме "Создание объектов, обработка изображений"

--- lesson_015/hw_01.py
+++ lesson_015/hw_01.py
@@ -6,13 +6,12 @@ logo = np.zeros((450, 450, 3), dtype=np.uint8)
 dict_points = {
     'point_1': (logo.shape[1] // 3, logo.shape[0] // 3),
     'point_2': (int(logo.shape[1] // 2.5), logo.shape[0] // 3),
-}
+    'point_3': (logo.shape[1] // 3, int(logo.shape[0] // 3 * 1.5))}
 
 print(dict_points['point_2'])
 
-cv2.line(logo, (dict_points['point_1'][0], dict_points['point_1'][1]),
-         (dict_points['point_2'][0], dict_points['point_2'][1]),
-         (255, 255, 255), thickness=3)
+cv2.line(logo, dict_points['point_1'], dict_points['point_2'], (255, 255, 255), thickness=3)
+cv2.line(logo, dict_points['point_1'], dict_points['point_3'], (255, 0, 0), thickness=1)
 
 
 cv2.imshow('Logo', logo)
