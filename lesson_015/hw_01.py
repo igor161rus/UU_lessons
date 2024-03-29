e698dccd08409fe7a89fcc5302ea91320ef702a4
igor <dranyi.igor@gmail.com>
2024-03-30 01:55:33 +0300

    Домашнее задание по теме "Создание объектов, обработка изображений"

--- lesson_015/hw_01.py
+++ lesson_015/hw_01.py
@@ -1,18 +1,31 @@
 import cv2
 import numpy as np
 
-logo = np.zeros((450, 450, 3), dtype=np.uint8)
+logo = np.zeros((480, 480, 3), dtype=np.uint8)
+python_logo = cv2.imread("python.png")
+python_logo = cv2.resize(python_logo, (logo.shape[1] // 3, logo.shape[0] // 4))
 
+logo[0:python_logo.shape[0], 0:python_logo.shape[1]] = python_logo
 dict_points = {
-    'point_1': (logo.shape[1] // 3, logo.shape[0] // 3),
-    'point_2': (int(logo.shape[1] // 2.5), logo.shape[0] // 3),
-    'point_3': (logo.shape[1] // 3, int(logo.shape[0] // 3 * 1.5))}
+    'point_1': (logo.shape[1] // 6, logo.shape[0] // 4),
+    'point_2': (logo.shape[1] // 3, logo.shape[0] // 4),
+    'point_3': (int(logo.shape[1] // 1.5), logo.shape[0] // 4),
+    'point_4': (int(logo.shape[1] // 1.2), logo.shape[0] // 4),
+}
 
-print(dict_points['point_2'])
+cv2.line(logo, dict_points['point_1'], dict_points['point_2'], (0, 0, 255), thickness=2)
+cv2.line(logo, dict_points['point_3'], dict_points['point_4'], (0, 0, 255), thickness=2)
+for i in dict_points:
+    cv2.line(logo, dict_points[i], (dict_points[i][0], dict_points[i][1] * 2), (255, 0, 0), thickness=2)
 
-cv2.line(logo, dict_points['point_1'], dict_points['point_2'], (255, 255, 255), thickness=3)
-cv2.line(logo, dict_points['point_1'], dict_points['point_3'], (255, 0, 0), thickness=1)
+cv2.ellipse(logo, (logo.shape[1] // 2, logo.shape[0] // 2),
+            (dict_points['point_1'][0], dict_points['point_1'][0]),
+            0, 0, 180, (0, 255, 0), thickness=2)
+cv2.ellipse(logo, (logo.shape[1] // 2, logo.shape[0] // 2),
+            (dict_points['point_2'][0], dict_points['point_2'][0]),
+            0, 0, 180, (0, 255, 0), thickness=2)
 
+cv2.circle(logo, (logo.shape[1] // 2, logo.shape[0] // 2), logo.shape[1] // 2, (255, 255, 0), thickness=2)
 
 cv2.imshow('Logo', logo)
 cv2.waitKey(0)
