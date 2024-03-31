362d894324c662cd5e033d4b3f5ef823a361fa1e
igor <dranyi.igor@gmail.com>
2024-03-31 23:15:04 +0300

    Домашнее задание по теме "Создание объектов, обработка изображений"

--- lesson_015/hw_01.py
+++ lesson_015/hw_01.py
@@ -1,12 +1,21 @@
 import cv2
 import numpy as np
 
+
+def get_gradient_2d(start, stop, width, height, is_horizontal):
+    if is_horizontal:
+        return np.tile(np.linspace(start, stop, width), (height, 1))
+    else:
+        return np.tile(np.linspace(start, stop, height), (width, 1)).T
+
+
 logo = np.zeros((480, 480, 3), dtype=np.uint8)
+logo[:, :, 0] = get_gradient_2d(0, 150, logo.shape[0], logo.shape[0], True)
 python_logo = cv2.imread("python.png")
 python_logo = cv2.resize(python_logo, (logo.shape[1] // 3, logo.shape[0] // 4))
 x, y = logo.shape[1] // 2 - python_logo.shape[1] // 2, logo.shape[0] // 2 - python_logo.shape[0] // 2
 h, w = python_logo.shape[:2]
-logo[y:y+h, x:x+w] = python_logo
+logo[y:y + h, x:x + w] = python_logo
 
 dict_points = {
     'point_1': (logo.shape[1] // 6, logo.shape[0] // 4),
