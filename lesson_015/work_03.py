10482fc1fc90aaeb020bc01a665f73c3ad2c8e14
igor161rus <dranyi.igor@gmail.com>
2024-04-01 12:22:01 +0300

    Обработка изображений и видео.

--- lesson_015/work_03.py
+++ lesson_015/work_03.py
@@ -4,3 +4,31 @@ import cv2
 # cv2.imshow('image', img)
 # cv2.waitKey(1000)
 
+# cap = cv2.VideoCapture('video/road.mov')
+# while True:
+#     success, frame = cap.read()
+#     cv2.imshow('video', frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+# cap = cv2.VideoCapture(0)
+# cap.set(3, 640)
+# cap.set(4, 480)
+#
+# while True:
+#     ret, frame = cap.read()
+#     cv2.imshow('webcam', frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+img = cv2.imread('images/opencv.png')
+# print(img.shape) # (512, 414, 3)
+# new_img = cv2.resize(img, (300, 200))
+# img = cv2.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
+img = cv2.resize(img, (int(img.shape[1] * 0.5), int(img.shape[0] * 0.5)))
+cv2.imshow('image', img[0:100, 0:200])
+
+# new_img = cv2.resize(img, (int(img.shape[1] * 0.5), int(img.shape[0] * 0.5)))
+# cv2.imshow('image', new_img)
+# cv2.imshow('image', img)
+cv2.waitKey(1000)
