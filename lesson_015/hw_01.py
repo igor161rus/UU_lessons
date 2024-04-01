03e16d331ba9fe1318089313266698c19b68ebe2
Igor <dranyi.igor@gmail.com>
2024-04-01 10:02:25 +0300

    Домашнее задание по теме "Создание объектов, обработка изображений"

--- lesson_015/hw_01.py
+++ lesson_015/hw_01.py
@@ -1,21 +1,7 @@
 import cv2
 import numpy as np
 
-
-def get_gradient_2d(start, stop, width, height, is_horizontal):
-    if is_horizontal:
-        return np.tile(np.linspace(start, stop, width), (height, 1))
-    else:
-        return np.tile(np.linspace(start, stop, height), (width, 1)).T
-
-
 logo = np.zeros((480, 480, 3), dtype=np.uint8)
-logo[:, :, 0] = get_gradient_2d(0, 150, logo.shape[0], logo.shape[0], True)
-python_logo = cv2.imread("python.png")
-python_logo = cv2.resize(python_logo, (logo.shape[1] // 3, logo.shape[0] // 4))
-x, y = logo.shape[1] // 2 - python_logo.shape[1] // 2, logo.shape[0] // 2 - python_logo.shape[0] // 2
-h, w = python_logo.shape[:2]
-logo[y:y + h, x:x + w] = python_logo
 
 dict_points = {
     'point_1': (logo.shape[1] // 6, logo.shape[0] // 4),
@@ -23,6 +9,20 @@ dict_points = {
     'point_3': (int(logo.shape[1] // 1.5), logo.shape[0] // 4),
     'point_4': (int(logo.shape[1] // 1.2), logo.shape[0] // 4),
 }
+
+i = 0
+k = 0
+for _ in range(50):
+    logo[:, 0 + i:50 + i] = (255 - k, 255 - k, 255 - k)
+    i += 20
+    k += 5
+
+python_logo = cv2.imread("python_logo.png")
+python_logo = cv2.resize(python_logo, (logo.shape[1] // 4, logo.shape[0] // 4))
+x, y = logo.shape[1] // 2 - python_logo.shape[1] // 2, logo.shape[0] // 2 - python_logo.shape[0] // 2
+h, w = python_logo.shape[:2]
+logo[y:y + h, x:x + w] = python_logo
+
 
 cv2.line(logo, dict_points['point_1'], dict_points['point_2'], (0, 0, 255), thickness=2)
 cv2.line(logo, dict_points['point_3'], dict_points['point_4'], (0, 0, 255), thickness=2)
