af2b29dc91d3ef0417f6995394a6db4d18ad726e
igor161rus <dranyi.igor@gmail.com>
2024-04-01 13:56:21 +0300

    Обработка изображений и видео.

--- lesson_015/work_03.py
+++ lesson_015/work_03.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # img = cv2.imread('images/opencv.png')
 # cv2.imshow('image', img)
@@ -28,6 +29,14 @@ img = cv2.imread('images/opencv.png')
 img = cv2.resize(img, (int(img.shape[1] * 0.5), int(img.shape[0] * 0.5)))
 # cv2.imshow('image', img[0:100, 0:200])
 img = cv2.GaussianBlur(img, (9, 9), 0)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.Canny(img, 30, 30)
+
+kernel = np.ones((5, 5), np.uint8)
+img = cv2.dilate(img, kernel, iterations=1)
+
+img = cv2.erode(img, kernel, iterations=1)
+
 # new_img = cv2.resize(img, (int(img.shape[1] * 0.5), int(img.shape[0] * 0.5)))
 # cv2.imshow('image', new_img)
 cv2.imshow('image', img)
