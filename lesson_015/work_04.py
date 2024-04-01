1ba789048e96311f554215067bfe15c50e8382b0
igor161rus <dranyi.igor@gmail.com>
2024-04-01 15:30:26 +0300

    Трансформация изображений с нейросетями

--- lesson_015/work_04.py
+++ lesson_015/work_04.py
@@ -1,14 +1,15 @@
 import cv2
 import numpy as np
 
-img = cv2.VideoCapture('video/road.mov')
+img = cv2.VideoCapture('video/sample_video.mp4 (240p).mp4')
 
 while True:
-    success, frame = img.read()
+    success, img = img.read()
 
 # img = cv2.resize(img, (int(img.shape[1] * 0.5), int(img.shape[0] * 0.5)))
     img = cv2.GaussianBlur(img, (9, 9), 0)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
     img = cv2.Canny(img, 30, 30)
 
     kernel = np.ones((5, 5), np.uint8)
@@ -16,7 +17,9 @@ while True:
 
     img = cv2.erode(img, kernel, iterations=1)
 
-    cv2.imshow('image', img)
+    cv2.imshow('result', img)
+
+    # cv2.waitKey(0)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
