27775560d7f90b32b573bc50fa8a39180d131c2c
igor161rus <dranyi.igor@gmail.com>
2024-04-02 15:25:43 +0300

    Домашнее задание по теме "Преобразование изображений. Цветовые форматы."

--- lesson_015/hw_02.py
+++ lesson_015/hw_02.py
@@ -6,7 +6,6 @@ new_img = np.zeros(img.shape, np.uint8)
 # new_img = np.zeros((170, 340), np.uint8)
 img_tmp = np.zeros(img.shape, np.uint8)
 
-
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 mask = cv2.inRange(hsv, (1, 1, 1), (255, 250, 250))
 img = cv2.bitwise_and(img, img, mask=mask)
@@ -17,48 +16,25 @@ con, hir = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 cv2.drawContours(new_img, con, -1, (0, 255, 0), 1)
 print(new_img.shape)
 
-#
-# cropped_image = img[0:170, 0:img.shape[1]]
-# con, hir = cv2.findContours(cropped_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-# cv2.drawContours(new_img, con, -1, (0, 0, 255), 1)
-# # print(con)
-# #
-# cropped_image_2 = img[170:340, 0:img.shape[1]]
-# con, hir = cv2.findContours(cropped_image_2, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-# cv2.drawContours(img_tmp, con, -1, (255, 0, 0), 1)
+height, width, channels = new_img.shape
 
+green = [0, 255, 0]
+red = [0, 0, 255]
+blue = [255, 0, 0]
 
-# print(con)
-# cropped_image = cropped_image[:, :, 255]
+for x in range(0, width):
+    for y in range(0, 170):
+        channels_xy = new_img[y, x]
+        # print(channels_xy)
+        if all(channels_xy == green):
+            new_img[y, x] = red
 
-# vis = np.concatenate((new_img, img_tmp), axis=0)
+    for y in range(305, 720):
+        channels_xy = new_img[y, x]
+        # print(channels_xy)
+        if all(channels_xy == green):
+            new_img[y, x] = blue
 
-
-# # new_img = cv2.cvtColor(new_img_mask, cv2.COLOR_BGR2GRAY)
-# new_img = cv2.Canny(new_img, 250, 250)
-# con, hir = cv2.findContours(new_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-# cv2.drawContours(new_img_1, con, -1, (255, 255, 0), 1)
-#
-# print(new_img_1.shape)
-# cropped_image = new_img_1[0:170, 0:new_img_1.shape[1]]
-# cropped_image = cropped_image[:, :, 0]
-# # cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_GRAY2BGR)
-# b, g, r = cv2.split(cropped_image)
-
-# print(img.shape)
-# height, width, channels = img.shape
-#
-# white = [255,255,255]
-# red = [0,0,255]
-#
-# for x in range(0,width):
-#     for y in range(0,170):
-#         channels_xy = img[y,x]
-#         if all(channels_xy == white):
-#             img[y,x] = red
-
-        # elif all(channels_xy == black):
-        #     img[y,x] = white
 
 cv2.imshow('image', new_img)
 cv2.waitKey(0)
