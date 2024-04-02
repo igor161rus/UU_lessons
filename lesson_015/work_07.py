1e5f840d103e1b7f46c16535b417bd470b9e21b6
Igor <dranyi.igor@gmail.com>
2024-04-02 16:54:17 +0300

    Распознавание лиц

--- lesson_015/work_07.py
+++ lesson_015/work_07.py
@@ -2,11 +2,11 @@
 
 import cv2
 img = cv2.imread('images/people.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = cv2.CascadeClassifier('faces.xml')
 
-result = faces.detectMultiScale(img, scaleFactor=2, minNeighbors=7)
+result = faces.detectMultiScale(gray, scaleFactor=2, minNeighbors=7)
 
 for (x, y, w, h) in result:
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=3)
