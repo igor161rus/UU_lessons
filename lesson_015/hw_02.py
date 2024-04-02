1bd23fd2b5fcb60f4892b116488da4b11627e71b
igor161rus <dranyi.igor@gmail.com>
2024-04-02 15:12:51 +0300

    Домашнее задание по теме "Преобразование изображений. Цветовые форматы."

--- lesson_015/hw_02.py
+++ lesson_015/hw_02.py
@@ -15,6 +15,7 @@ img = cv2.GaussianBlur(img, (7, 7), 0)
 img = cv2.Canny(img, 250, 250)
 con, hir = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 cv2.drawContours(new_img, con, -1, (0, 255, 0), 1)
+print(new_img.shape)
 
 #
 # cropped_image = img[0:170, 0:img.shape[1]]
