34e5dceddbbf8110e0fcd7123013e7ea756e753e
igor161rus <dranyi.igor@gmail.com>
2024-04-04 16:14:37 +0300

    Домашнее задание по теме "Распознавание объектов"

--- lesson_015/hw_03.py
+++ lesson_015/hw_03.py
@@ -4,66 +4,32 @@ import numpy as np
 left_eye_x = 0
 right_eye_x = 0
 
-cap = cv2.VideoCapture('video/videoplayback.mp4')
+cap = cv2.VideoCapture('video/test_1.mp4')
 
 while True:
     success, img = cap.read()
-
+    cut_eye = np.zeros(img.shape, np.uint8)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     eyes_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
     faces_cascade = cv2.CascadeClassifier('faces.xml')
 
-    faces = faces_cascade.detectMultiScale(gray, scaleFactor=2, minNeighbors=7)
-    # eyes = eyes_cascade.detectMultiScale(gray, scaleFactor=5, minNeighbors=2)
+    faces = faces_cascade.detectMultiScale(gray, scaleFactor=1.4, minNeighbors=6)
 
-    # for (x, y, w, h) in faces:
-    #     k1 = y + h // 3
-    #     k2 = (y + h) // 2
-    #     faces1 = cv2.rectangle(img, (x, y + h // 3), (x + w, (y + h) // 2), (0, 255, 0), thickness=1)
-    #     # mask = cv2.inRange(faces, (1, 1, 1), (255, 250, 250))
-    #     # gray = cv2.bitwise_and(img, img, mask=mask)
-    #     x1 = faces1[0][0][0]
-    #     y1 = faces1[0][1][0]
-    #     w1 = faces1[0][2][0]
-    #     h1 = faces1[0][3][0]
-    #     print(x1, y1, w1, h1)
-    #     faces1[y:y1 + h1, x:x1 + w1] = cv2.medianBlur(gray[y:y1 + h1, x:x1 + w1], 35)
-    #     # faces1[y:y+h, x:x+w] = cv2.medianBlur(faces[y+h:, x:x+w], 35)
+    for (x, y, w, h) in faces:
+        eyes = eyes_cascade.detectMultiScale(gray, scaleFactor=2, minNeighbors=6)
+        if len(eyes) > 2:
+            ex1, ey1, ew1, eh1 = eyes[0]
+            ex2, ey2, ew2, eh2 = eyes[1]
+            if faces[0][0] < ex1:
+                eye = cv2.rectangle(img, (x, ey1), (x + w, ey1 + eh2), (0, 0, 0), thickness=1)
+                cut_eye = eye[ey1:ey1 + eh2, x:x + w]
+                cut_eye = cv2.GaussianBlur(cut_eye, (49, 49), 0)
+                # eye[ey1:ey1 + eh2, x:x + w] = cv2.medianBlur(eye[ey1:ey1 + eh2, x:x + w], 35)
+                cut_eye = cv2.cvtColor(cut_eye, cv2.COLOR_BGR2GRAY)
+                cut_eye = cv2.cvtColor(cut_eye, cv2.COLOR_GRAY2BGR)
+                img[ey1:ey1 + eh2, x:x + w] = cut_eye
 
-    # for (x, y, w, h) in eyes:
-    #     eye = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), thickness=1)
-    #     eye[y:y + h, x:x + w] = cv2.medianBlur(eye[y:y + h, x:x + w], 35)
-    # for (ex, ey, ew, eh) in eyes:  # draws boxes around eyes
-
-    for (x, y, w, h) in faces:  # draws box around face
-        print('face', x, y, w, h)
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)  #
-        roi_gray = gray[y:y + h, x:x + w]
-        # roi_color = img[y:y + h, x:x + w]
-        eyes = eyes_cascade.detectMultiScale(gray, scaleFactor=2, minNeighbors=2)  # looks for eyes
-        for (ex, ey, ew, eh) in eyes:  # draws boxes around eyes
-            eye = cv2.rectangle(img, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), thickness=1)
-            print('eye rect')
-            print(eyes, eyes[0], eyes[1])
-            print(len(eyes))
-            print('face', x, y, w, h)
-            print('eye', ex, ey, ew, eh)
-
-#            if (ex + ey) / 2 < (x + y) / 2:
-            print(f'for lef: {eyes[0][0]} < {(x+w) / 2} ')
-            print(f'for right: {eyes[1][0]} < {(x+w) / 2} ')
-
-            if eyes[0][0] < (x + w) / 2:
-                left_eye_x = eyes[0][0]
-                print('left', left_eye_x)
-            # elif (ex + ey) / 2 > (x + y) / 2:
-            if eyes[1][0] > (x + w) / 2:
-                right_eye_x = eyes[1][0]
-                print('right', right_eye_x)
-            print('********************')
-            cv2.line(img, (int(left_eye_x), int(right_eye_x)),
-                     (int(left_eye_x), int(right_eye_x)), (0, 255, 0), 10)
     cv2.imshow('result', img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
