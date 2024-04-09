6ace701e2596608b745a52395d90a3dcd1e3a0fa
igor161rus <dranyi.igor@gmail.com>
2024-04-09 11:41:37 +0300

    Задача №1. Реализовать функционал: Вывод средней цены за период

--- Internship/sprint_01/task_01/calculation.py
+++ Internship/sprint_01/task_01/calculation.py
@@ -6,6 +6,7 @@ def calculate_and_display_average_price(data):
        Returns:
            None
     """
-    # data.to_csv('data.csv', index=False)
+    # data.to_csv('data.csv', index=False) #для проверки, что там вообще есть
+    # https://finance.yahoo.com/quote/AAPL/history?period1=1709942400&period2=1712620800&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true
     average_price = data['Close'].mean()
     print(f'Средняя цена: {average_price:.2f} USD')
