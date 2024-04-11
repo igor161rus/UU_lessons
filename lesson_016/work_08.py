c3bfe72382dbf9898e4b3f1ebf9f6f9339737ed4
igor161rus <dranyi.igor@gmail.com>
2024-04-11 16:20:43 +0300

    Базы данных. SQLife3.

--- lesson_016/work_08.py
+++ lesson_016/work_08.py
@@ -1,1 +1,12 @@
 import  sqlite3
+
+conn = sqlite3.connect("data/base.sqlite")
+# conn.text_factory = bytes
+cur = conn.cursor()
+cur.execute("select * from Orders where (Freight > 100) and (ShipRegion = 'RJ')")
+results = cur.fetchall()
+print(results)
+
+cur.execute('select ContactName from Customers where ContactName like "%C%"')
+res = cur.fetchall()
+print(res)
