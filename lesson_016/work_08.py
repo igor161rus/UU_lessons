6f9930ccfa07ef625b2b2ec622285036b9ec84e3
igor161rus <dranyi.igor@gmail.com>
2024-04-12 09:28:48 +0300

    Базы данных. SQLife3.

--- lesson_016/work_08.py
+++ lesson_016/work_08.py
@@ -10,3 +10,20 @@ print(results)
 cur.execute('select ContactName from Customers where ContactName like "%C%"')
 res = cur.fetchall()
 print(res)
+
+cur.execute('delete from Orders where OrderID = "10"')
+conn.commit()
+
+cur.execute('insert into Orders (OrderID, CustomerID, EmployeeID) values ("10","Anton","5")')
+# cur.execute('insert into Orders values ("10","2020-10-10","RJ","1000")')
+conn.commit()
+
+cur.execute('select * from Orders')
+res = cur.fetchall()
+print(res)
+
+cur.execute('select * from Orders where OrderID = "10"')
+res = cur.fetchall()
+print(res)
+
+conn.close()
