83e66e242eb059bb939403ea6ffaa3746e73e96a
igor161rus <dranyi.igor@gmail.com>
2024-04-15 09:54:59 +0300

    Базы данных. Работа через ORM.

--- lesson_016/work_09.py
+++ lesson_016/work_09.py
@@ -22,3 +22,40 @@ class Album(BaseTable):
 
 
 db.create_tables([Artist, Album])
+
+new_artist = Artist(name='Led Zeppelin')
+new_artist.save()
+# new_album = Album.create(artist=new_artist, title='Led Zeppelin IV',
+#                          release_date=datetime.datetime(1971, 11, 1),
+#                          publisher='Atlantic', media_type='LP')
+
+albums = [
+    {
+        'artist': 'Led Zeppelin',
+        'title': 'Led Zeppelin III',
+        'release_date': datetime.datetime(1970, 10, 22),
+        'publisher': 'Atlantic',
+        'media_type': 'LP'
+    },
+    {
+        'artist': 'Led Zeppelin',
+        'title': 'Led Zeppelin II',
+        'release_date': datetime.datetime(1969, 11, 8),
+        'publisher': 'Atlantic',
+        'media_type': 'LP'
+    },
+    {
+        'artist': 'Led Zeppelin',
+        'title': 'Led Zeppelin I',
+        'release_date': datetime.datetime(1968, 10, 22),
+        'publisher': 'Atlantic',
+        'media_type': 'LP'
+    }
+]
+
+albums_in_db = Album.insert_many(albums).execute()
+
+bands = ['Led Zeppelin', 'Pink Floyd', 'Rolling Stones']
+for band in bands:
+    Artist.create(name=band)
+
