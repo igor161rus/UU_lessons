71a273b84c6792482d05be955c23e129119e1049
Igor <dranyi.igor@gmail.com>
2024-04-19 17:00:13 +0300

    Курсовой проект: подключение ORM

--- course_project/models.py
+++ course_project/models.py
@@ -1,6 +1,11 @@
-from pony.orm import Database
+from pony.orm import Database, Required, Json
 from settings import DB_CONFIG
 
 db = Database()
 db.bind(**DB_CONFIG)
 
+
+class UserState(db.Entity):
+    scenario_name = Required(str)
+    step_name = Required(int)
+    context = Required(Json)
