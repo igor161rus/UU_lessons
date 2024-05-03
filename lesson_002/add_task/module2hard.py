583515a90e938366ed1ebf96db395e74b32e835e
Igor <dranyi.igor@gmail.com>
2024-05-03 18:42:13 +0300

    Дополнительное практическое задание по модулю

--- lesson_002/add_task/module2hard.py
+++ lesson_002/add_task/module2hard.py
@@ -2,23 +2,14 @@ from random import randint
 
 a = randint(3, 20)
 
-# 11 - 11029384756
+print(str(a) + ' - число из первой вставки')
+pair = ''
+b = 0
+for i in range(1, a):
+    for j in range(1, a):
+        if j > i:
+            b = i + j
+            if a % b == 0:
+                pair = pair + str(i) + str(j)
 
-a = 11
-
-def multiple_pair(a):
-    print(a)
-    pair = ''
-    b = 0
-    for i in range(1, a):
-        for j in range(1, a):
-            if j > i:
-                b = i + j
-                if a % b == 0:
-                    print(i, ' ', j)
-                    pair = pair + str(i) + str(j)
-    return pair
-
-
-print(multiple_pair(a))
-print('11029384756')
+print(pair + ' - нужный пароль')
