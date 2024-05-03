69cf3e2c632f2d095f3abd568c3cbe09fce1a55c
igor161rus <dranyi.igor@gmail.com>
2024-05-03 14:32:51 +0300

    Дополнительное практическое задание по модулю

--- lesson_002/add_task/module2hard.py
+++ lesson_002/add_task/module2hard.py
@@ -1,15 +1,20 @@
 from random import randint
 
 a = randint(3, 20)
+
+
 def multiple_pair(a):
     print(a)
+    pair = ''
     b = 0
     for i in range(0, a):
         for j in range(1, a):
             if j < i:
                 b = i + j
                 if a % b == 0:
-                    print(i, ' ', j)
-    return b
+                    print(j, ' ', i)
+                    pair = str(j) + str(i) + pair
+    return pair
+
 
 print(multiple_pair(a))
