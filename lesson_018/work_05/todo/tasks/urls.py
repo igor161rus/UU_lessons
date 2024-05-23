243bf5770f652b9e1a2fd4adfc325232226cd2e8
igor161rus <dranyi.igor@gmail.com>
2024-05-23 15:23:57 +0300

    django

--- lesson_018/work_05/todo/tasks/urls.py
+++ lesson_018/work_05/todo/tasks/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.task_list, name='task_list'),
+    path('about/', views.AboutView.as_view(), name='about'),
 ]
--- lesson_018/work_05/todo/tasks/views.py
+++ lesson_018/work_05/todo/tasks/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from django.views.generic import TemplateView
 
 
 def task_list(request, *args, **kwargs):
+    tasks = ['Task 1', 'Task 2', 'Task 3']
     # return HttpResponse('<ul>'
     #                     '<li>Task 1</li>'
     #                     '<li>Task 2</li>'
     #                     '<li>Task 3</li>'
     #                     '</ul>'
     #                     )
-    return render(request, 'task_list.html', {})
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+
+class AboutView(TemplateView):
+    template_name = 'tasks/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Tasks'
+        context['title'] = 'About'
+        context['description'] = 'About Tasks'
+        return context
