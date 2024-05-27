582807539347aab0301a9b5f80b6c5fd971395f0
igor161rus <dranyi.igor@gmail.com>
2024-05-27 16:48:31 +0300

    Задача №2. Инверсия цветов изображения

--- Internship/sprint_03/task_02/bot.py
+++ Internship/sprint_03/task_02/bot.py
@@ -68,6 +68,10 @@ def pixelate_image(image, pixel_size):
     )
     return image
 
+# Инвертируем цвета
+def invert_colors(image):
+    return ImageOps.invert(image)
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -77,9 +81,11 @@ def send_welcome(message):
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
     user_states[message.chat.id] = {'level': 0}
+    # user_states[message.chat.id]['photo'] = message.photo[-1].file_id
     bot.reply_to(message, "I got your photo! Please choose what you'd like to do with it.",
                  reply_markup=get_options_keyboard(message))
-    user_states[message.chat.id] = {'photo': message.photo[-1].file_id}
+    user_states[message.chat.id]['photo'] = message.photo[-1].file_id
+    user_states[message.chat.id]['message_id'] = message.message_id
 
 
 def get_options_keyboard(message):
@@ -92,8 +98,8 @@ def get_options_keyboard(message):
         return keyboard
     elif user_states.get(message.chat.id) and user_states[message.chat.id]['level'] == 1:
         keyboard = types.InlineKeyboardMarkup()
-        pixelate_btn = types.InlineKeyboardButton("Pixelate", callback_data="pixelate")
-        ascii_btn = types.InlineKeyboardButton("ASCII Art", callback_data="ascii")
+        pixelate_btn = types.InlineKeyboardButton("Pixelate img", callback_data="pixelate_img")
+        ascii_btn = types.InlineKeyboardButton("Invert color", callback_data="invert")
         keyboard.add(pixelate_btn, ascii_btn)
         return keyboard
     elif user_states.get(message.chat.id) and user_states[message.chat.id]['level'] == 2:
@@ -107,11 +113,23 @@ def get_options_keyboard(message):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     chat_id = call.message.chat.id
-
     if call.data == "pixelate":
         user_states[chat_id]['level'] = 1
+        bot.delete_message(chat_id, user_states[chat_id]['message_id'] + 1)
+        bot.reply_to(call.message, "Выберите действие...", reply_markup=get_options_keyboard(call.message))
+        # pixelate_and_send(call.message)
+    elif call.data == "pixelate_img":
+        user_states[chat_id]['level'] = 3
         bot.answer_callback_query(call.id, "Pixelating your image...")
         pixelate_and_send(call.message)
+    elif call.data == "invert":
+        user_states[chat_id]['level'] = 4
+        bot.reply_to(call.message, "Inverting your image...",
+                     reply_markup=get_options_keyboard(call.message))
+        # bot.answer_callback_query(call.id, "Inverting your image...")
+        # invert_and_send(call.message)
+        pixelate_and_send(call.message)
+
     elif call.data == "ascii":
         user_states[chat_id]['ascii'] = True
         user_states[chat_id]['level'] = 2
@@ -129,8 +147,10 @@ def pixelate_and_send(message):
 
     image_stream = io.BytesIO(downloaded_file)
     image = Image.open(image_stream)
-    pixelated = pixelate_image(image, 20)
-
+    if user_states.get(message.chat.id) and user_states[message.chat.id]['level'] == 3:
+        pixelated = pixelate_image(image, 20)
+    elif user_states.get(message.chat.id) and user_states[message.chat.id]['level'] == 4:
+        pixelated = invert_colors(image)
     output_stream = io.BytesIO()
     pixelated.save(output_stream, format="JPEG")
     output_stream.seek(0)
