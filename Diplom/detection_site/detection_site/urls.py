7d215b190c4281069297c402f873ad1f85604dd9
igor161rus <dranyi.igor@gmail.com>
2024-05-29 15:58:31 +0300

    Дипломная работа по курсу "Python-разработчик"

--- Diplom/detection_site/detection_site/urls.py
+++ Diplom/detection_site/detection_site/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('object_detection/', include('object_detection.urls')),
+    path('', RedirectView.as_view(url='/object_detection/', permanent=True)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Diplom/detection_site/object_detection/admin.py
+++ Diplom/detection_site/object_detection/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ImageFeed, DetectedObject
 
-# Register your models here.
+admin.site.register(ImageFeed)
+admin.site.register(DetectedObject)
