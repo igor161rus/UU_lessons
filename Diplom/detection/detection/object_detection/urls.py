2ffea874e2c40c151c8f405802b4fbb49e65eb02
igor161rus <dranyi.igor@gmail.com>
2024-06-04 16:15:59 +0300

    Диплом

--- Diplom/detection/detection/object_detection/urls.py
+++ Diplom/detection/detection/object_detection/urls.py
@@ -4,7 +4,8 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('dashboard/', dashboard, name='dashboard'),
-    path('login/', login, name='login'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('about/', about, name='about'),
     path('image/<int:pk>/', image, name='image'),
--- Diplom/detection/detection/object_detection/views.py
+++ Diplom/detection/detection/object_detection/views.py
@@ -3,17 +3,19 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout, login
 from django.urls import reverse_lazy
 
 from .models import *
 from .utils import *
 from .forms import *
 
-menu = [
-    {"title": "Главная", "url_name": "home"},
-    {"title": "Приборная доска", "url_name": "dashboard"},
-    {"title": "Войти", "url_name": "login"},
-]
+# menu = [
+#     {"title": "Главная", "url_name": "home"},
+#     {"title": "Приборная доска", "url_name": "dashboard"},
+#     {"title": "Войти", "url_name": "login"},
+# ]
 
 
 def home(request):
@@ -35,8 +37,8 @@ def dashboard(request):
     return HttpResponse("dashboard")
 
 
-def login(request):
-    return HttpResponse("login")
+# def login(request):
+#     return HttpResponse("login")
 
 
 def image(request, pk):
@@ -57,8 +59,9 @@ def process_image_feed(request, feed_id):
 def pageNotFound(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
+
 class RegisterUser(DataMixin, CreateView):
-    form_class = UserCreationForm
+    form_class = RegisterUserForm
     template_name = 'object_detection/register.html'
     success_url = reverse_lazy('login')
 
@@ -66,3 +69,26 @@ class RegisterUser(DataMixin, CreateView):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Регистрация")
         return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'object_detection/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
