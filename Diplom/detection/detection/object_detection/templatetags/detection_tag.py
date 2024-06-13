c53d338706f4139049d8791d6514961644147529
Igor <dranyi.igor@gmail.com>
2024-06-14 00:39:55 +0300

    Диплом

--- Diplom/detection/detection/object_detection/templatetags/detection_tag.py
+++ Diplom/detection/detection/object_detection/templatetags/detection_tag.py
@@ -10,3 +10,9 @@ register = template.Library()
 def current_stat(id):
     stat = DetectedObject.objects.filter(image_feed=id).values('object_type', 'method_detected').annotate(Count('object_type'))
     return stat
+
+
+@register.simple_tag
+def get_all_type():
+    type_detected = DetectedObject.objects.order_by('object_type').values('object_type').distinct()
+    return type_detected
