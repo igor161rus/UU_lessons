cbe887901c9678abb1d43d2c0c65a568f6373ff0
Igor <dranyi.igor@gmail.com>
2024-06-22 01:47:53 +0300

    Диплом

--- Diplom/detection/detection/object_detection/management/commands/bot.py
+++ Diplom/detection/detection/object_detection/management/commands/bot.py
@@ -1,20 +1,27 @@
-import sqlite3
-
+from django.core.management.base import BaseCommand
+from django.conf import settings
 import telebot
-from PIL import Image
-import io
 from telebot import types
-from settings import TOKEN_BOT
+from ...models import *
 
-TOKEN = TOKEN_BOT
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(settings.TOKEN, threaded=False)
 
 user_states = {}
 
 
+class Command(BaseCommand):
+    # Используется как описание команды обычно
+    help = 'Just a command for launching a Telegram bot.'
+
+    def handle(self, *args, **kwargs):
+        bot.enable_save_next_step_handlers(delay=2)  # Сохранение обработчиков
+        bot.load_next_step_handlers()  # Загрузка обработчиков
+        bot.infinity_polling()
+
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Send me an image, and I'll provide options for you!")
+    bot.reply_to(message, "Привет! Я бот для обнаружения объектов. ")
 
 
 @bot.message_handler(content_types=['photo'])
@@ -45,11 +52,9 @@ def load_image(message):
     bot.reply_to(message, 'Фотография сохранена.')
 
 
-def insert_db(image, id):
-    con = sqlite3.connect('db.sqlite3')
-    cur = con.cursor()
-    cur.execute('''INSERT INTO object_detection_imagefeed VALUES (?), (?)''', (image, id))
+# def insert_db(image, id):
+#     con = sqlite3.connect('../../../db.sqlite3')
+#     cur = con.cursor()
+#     cur.execute('''INSERT INTO object_detection_imagefeed VALUES (?), (?)''', (image, id))
 
-
-
-bot.polling(none_stop=True)
+# bot.polling(none_stop=True)
