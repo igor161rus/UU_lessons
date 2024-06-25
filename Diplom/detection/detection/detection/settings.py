d8f7d5322fd6da33d432bf2f7ad371cc392dc810
Igor <dranyi.igor@gmail.com>
2024-06-25 15:15:22 +0300

    Diplom

--- Diplom/detection/detection/detection/settings.py
+++ Diplom/detection/detection/detection/settings.py
@@ -131,7 +131,7 @@ MEDIA_URL = '/media/'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'dranyi.igor@gmail.com'
-EMAIL_HOST_PASSWORD = 'My Key 72'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
