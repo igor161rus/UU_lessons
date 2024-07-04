06a1897d5c76a078569d763f4132a12051a1b90e
Igor <dranyi.igor@gmail.com>
2024-07-04 15:07:44 +0300

    Спринт 5, проект Джанго

--- Internship/sprint_05/task_01/urban_project/board/forms.py
+++ Internship/sprint_05/task_01/urban_project/board/forms.py
@@ -7,7 +7,8 @@ from django.contrib.auth.models import User
 class AdvertisementForm(forms.ModelForm):
     class Meta:
         model = Advertisement
-        fields = ['title', 'content', 'author']
+        fields = ['title', 'content', 'author', 'ImageField']
+
 
 class SignUpForm(UserCreationForm):
     class Meta:
