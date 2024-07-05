27d309ec5c04c9e112ebe5f9339b48e4c4d60e7a
Igor <dranyi.igor@gmail.com>
2024-07-05 16:31:02 +0300

    Спринт 5, проект Джанго

--- Internship/sprint_05/task_01/urban_project/board/views.py
+++ Internship/sprint_05/task_01/urban_project/board/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Advertisement
@@ -8,6 +10,9 @@ from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
 from django.contrib.auth import login, authenticate
+from django.views.generic.detail import DetailView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 def logout_view(request):
@@ -38,7 +43,6 @@ def advertisement_list(request):
 
 def advertisement_detail(request, pk):
     advertisement = Advertisement.objects.get(pk=pk)
-
 
     return render(request, 'board/advertisement_detail.html', {'advertisement': advertisement})
 
@@ -83,13 +87,29 @@ def delete_advertisement(request, pk):
     return render(request, 'board/delete_advertisement.html', {'advertisement': advertisement})
 
 
-def blog_post_like(request, pk):
-    post = get_object_or_404(Advertisement, id=request.POST.get('advertisement_id'))
-
+def post_like(request, pk):
+    post = get_object_or_404(Advertisement, id=request.POST.get('id'))
     if post.likes.filter(id=request.user.id).exists():
         post.likes.remove(request.user)
     else:
         post.likes.add(request.user)
 
-    return redirect('board:advertisement_detail', pk=pk)
+    return HttpResponseRedirect(reverse('board:advertisement_detail', args=[str(pk)]))
 
+
+class BlogPostDetailView(DetailView):
+    model = Advertisement
+
+    # template_name = MainApp/BlogPost_detail.html
+    # context_object_name = 'object'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+
+        likes_connected = get_object_or_404(Advertisement, id=self.kwargs['pk'])
+        liked = False
+        if likes_connected.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        data['number_of_likes'] = likes_connected.number_of_likes()
+        data['post_is_liked'] = liked
+        return data
