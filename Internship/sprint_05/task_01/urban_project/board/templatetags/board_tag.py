632cb94b3c547df76a99babe4ae1d56b7dbd5ccb
igor161rus <dranyi.igor@gmail.com>
2024-07-09 17:12:02 +0300

    sprint_05

---
+++ Internship/sprint_05/task_01/urban_project/board/templatetags/board_tag.py
@@ -0,0 +1,13 @@
+from django import template
+
+from ..models import *
+
+register = template.Library()
+
+
+@register.filter
+def get_likes_count():
+    user_id = 5
+    count = Advertisement.objects.filter(user_id=user_id).count()
+    print(111111, count)
+    return count
