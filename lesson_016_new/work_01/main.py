aa03bc64ebada46e4d7c8c0ef1b89fc4fcdd920a
Igor <dranyi.igor@gmail.com>
2024-12-11 16:12:00 +0300

    Спринт 5, проект Джанго

--- lesson_016_new/work_01/main.py
+++ lesson_016_new/work_01/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from pydantic import BaseModel
 from fastapi.responses import FileResponse
+from navigate_strategy import navigate_info
 
 # Создаем экземпляр приложения FastAPI
 app = FastAPI()
@@ -37,3 +38,25 @@ async def update_item(item_id: int, item: Item):
 @app.delete("/items/{item_id}")
 async def delete_item(item_id: int):
     return {"message": "Item deleted", "item_id": item_id}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    if name in navigate_info:
+        file_path = navigate_info[name]  # Get the path from your dictionary
+        # Important: Check if the file exists before trying to serve it
+        try:
+            with open(file_path, 'r'):
+                pass  # Simple file existence check
+            return FileResponse(file_path)  # Only serve if the file exists
+        except FileNotFoundError:
+            return {"error": f"HTML file '{file_path}' not found."}  # Handle file not found
+    else:
+        out = {"message": f"Hello {name}"}
+        return out
+
+
+@app.post("/hello/calculate")
+def calculate(num1: int = Form(ge=0, lt=111111), num2: int = Form(ge=0, lt=111111)):
+    print("num1 =", num1, "   num2 =", num2)
+    return {"result": num1 + num2}
---
+++ lesson_016_new/work_01/navigate_strategy.py
@@ -0,0 +1,7 @@
+"""
+Here we use pattern strategy to reduce code complexity.
+IE we don't need to define all html paths in the main file.
+"""
+navigate_info = {"cool_start": "html/cool_start.html",
+                 "calculate": "html/calculate.html",
+                 }
