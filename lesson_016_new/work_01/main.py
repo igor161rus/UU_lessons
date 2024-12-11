380b87510c3138366ab116321880112ee1a9fa46
igor161rus <dranyi.igor@gmail.com>
2024-12-11 10:43:00 +0300

    FastAPI

---
+++ lesson_016_new/work_01/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Создаем экземпляр приложения FastAPI
+app = FastAPI()
+
+
+# Определение базового маршрута
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, FastAPI!"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return {"name": item.name, "price": item.price}
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "name": item.name, "price": item.price}
+
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    return {"message": "Item deleted", "item_id": item_id}
